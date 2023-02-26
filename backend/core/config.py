7e1722114f8209c43399da4335090975057ec4b4
jeha00 <rudtls0611@naver.com>
2023-02-27 01:36:38 +0900

    Conf: initial_setting

--- backend/core/config.py
+++ backend/core/config.py
@@ -1,1 +1,31 @@
+from typing import List
+from pathlib import Path
+import secrets 
 
+from pydantic import BaseSettings, SecretStr, AnyHttpUrl
+
+
+BASE_DIR = Path(__file__).resolve().parents[2]
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = 'SNS'
+    API_V1_STR: str = '/api/v1'
+
+    DB_USERNAME: str = 'root'
+    DB_PASSWORD: SecretStr = 'a1s2s3d4'
+    DB_HOST: str = '127.0.0.1'
+    DB_PORT: int = '3306'
+    DB_NAME: str = 'SNS_02'
+    SQLALCHEMY_DATABASE_URI: str = "mysql+pymysql://{username}:{password}@{host}:{port}/{name}?charset=utf8mb4"
+
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SECRET_ALGORITHM: str = "HS256"
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+
+    BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+
+    class config:
+        env_file = BASE_DIR / 'env'
+
+
+settings = Settings()
--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,18 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+from core.config import settings
+from apis.api_v1.api import api_router
+
+app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_STR}")
+
+if settings.BACKEND_CORS_ORIGINS:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origin=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
+app.include_router(api_router, prefix=settings.API_V1_STR)
