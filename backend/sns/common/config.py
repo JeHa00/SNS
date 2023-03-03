f69c4d7cab4cb4e5c16f146cea94ab769967bca1
jeha00 <rudtls0611@naver.com>
2023-03-03 20:08:45 +0900

    update - add pree-commit hook - update group name in poetry - make path.py to use in common - exchange db url string to CONST in common.config.settings - exchange Exception to self.init_app() in get_db method

--- backend/sns/common/config.py
+++ backend/sns/common/config.py
@@ -1,22 +1,22 @@
-from typing import List
-from pathlib import Path
-import secrets 
-
 from pydantic import BaseSettings, SecretStr, AnyHttpUrl
+from typing import List
+import secrets
 
+from sns.common.path import BASE_DIR
 
-BASE_DIR = Path(__file__).resolve().parents[2]
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = 'SNS'
-    API_V1_STR: str = '/api/v1'
+    PROJECT_NAME: str = "SNS"
+    API_V1_STR: str = "/api/v1"
 
-    DB_USERNAME: str = 'root'
-    DB_PASSWORD: SecretStr = 'a1s2s3d4'
-    DB_HOST: str = '127.0.0.1'
-    DB_PORT: int = '3306'
-    DB_NAME: str = 'SNS_02'
-    SQLALCHEMY_DATABASE_URI: str = "mysql+pymysql://{username}:{password}@{host}:{port}/{name}?charset=utf8mb4"
+    DB_USERNAME: str = "root"
+    DB_PASSWORD: SecretStr = "a1s2s3d4"
+    DB_HOST: str = "127.0.0.1"
+    DB_PORT: int = "3306"
+    DB_NAME: str = "SNS"
+    DATABASE_URI: str = (
+        "mysql+pymysql://{username}:{pw}@{host}:{port}/{name}?charset=utf8mb4"
+    )
 
     SECRET_KEY: str = secrets.token_urlsafe(32)
     SECRET_ALGORITHM: str = "HS256"
@@ -25,7 +25,7 @@ class Settings(BaseSettings):
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
 
     class config:
-        env_file = BASE_DIR / 'env'
+        env_file = BASE_DIR / "env"
 
 
 settings = Settings()
--- backend/sns/common/session.py
+++ backend/sns/common/session.py
@@ -1,66 +1,68 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine 
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import logging
 
-from sns.common.config import settings 
+from sns.common.config import settings
+
 
 class SQLAlchemy:
     def __init__(self, app: FastAPI = None, **kwargs):
         self._engine = None
-        self._session = None 
+        self._session = None
         if app is not None:
             self.init_app(app=app, **kwargs)
 
-
     def init_app(self, app: FastAPI, **kwargs):
         """
-        db 초기화 함수  
+        db 초기화 함수
         """
         self._engine = create_engine(
-            "mysql+pymysql://{username}:{password}@{host}:{port}/{name}?charset=utf8mb4".format(
+            settings.DATABASE_URI.format(
                 username=settings.DB_USERNAME,
-                password=settings.DB_PASSWORD.get_secret_value(),
+                pw=settings.DB_PASSWORD.get_secret_value(),
                 host=settings.DB_HOST,
                 port=settings.DB_PORT,
                 name=settings.DB_NAME,
             )
         )
 
-        self._session = sessionmaker(autocommit=False, autoflush=False, bind=self._engine)
+        self._session = sessionmaker(
+            autocommit=False, autoflush=False, bind=self._engine
+        )
 
-        @app.on_event('startup')
+        @app.on_event("startup")
         def startup(self):
-            self._engine.connect() 
-            logging.info('DB connected')
+            self._engine.connect()
+            logging.info("DB connected")
 
-        @app.on_event('shutdown')
+        @app.on_event("shutdown")
         def shutdown(self):
             self._session.close_all()
-            self._engine.dispose() 
+            self._engine.dispose()
             logging.info("DB disconnected")
-
 
     def get_db(self):
         """
-        요청마다 DB 세션 유지하는 함수 
+        요청마다 DB 세션 유지하는 함수
         """
-        if self._session is None: 
-            raise Exception("must bt called 'init_app'")
+        if self._session is None:
+            self.init_app()
         try:
             db_session = self._session()
-            yield db_session 
+            yield db_session
         finally:
-            db_session.close()  
+            db_session.close()
 
-    @property 
+    @property
     def session(self):
-        return self.get_db 
+        return self.get_db
 
     @property
     def engine(self):
         return self._engine
+
 
 db = SQLAlchemy()
 Base = declarative_base()
