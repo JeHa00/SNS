d5302a1aef73a228689d37a14c13034cea72d5f8
jeha00 <rudtls0611@naver.com>
2023-03-15 22:50:57 +0900

    FEAT: created user table - added alembic dependency - changed Base and BaseMixin classes - added attributes of User class entity

--- backend/sns/users/model.py
+++ backend/sns/users/model.py
@@ -1,7 +1,6 @@
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, Boolean
 
-from sns.common.session import Base
-from sns.common.base import BaseMixin
+from sns.common.base import Base, BaseMixin
 
 
 class User(Base, BaseMixin):
@@ -10,3 +9,5 @@ class User(Base, BaseMixin):
     profile_text = Column(String(300), nullable=True)
     profile_image_name = Column(String(50), nullable=True)
     profile_image_path = Column(String(200), nullable=True)
+    verified = Column(Boolean, nullable=False, default=False)
+    verification_code = Column(String(10), nullable=True, unique=True)
