6c64d97abdaee9bf1ccfff192960c37e16ccaa82
jeha00 <rudtls0611@naver.com>
2023-04-03 22:59:17 +0900

    REFACTOR: refactor test_service logic about user domain

--- backend/sns/users/test/test_service.py
+++ backend/sns/users/test/test_service.py
@@ -4,11 +4,13 @@ from datetime import datetime, timedelta
 from jose import jwt
 
 from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import HTTPException
 
 from sns.users.test.utils import random_lower_string, random_email
 from sns.users.schema import UserCreate, UserUpdate
 from sns.common.config import settings
 from sns.users.service import (
+    get_current_user_verified,
     create_access_token,
     get_password_hash,
     get_current_user,
@@ -21,21 +23,12 @@ from sns.users.service import (
 )
 
 
-@pytest.fixture(scope="function")
-def fake_user(client, db_session):
-    email = random_email()
-    password = random_lower_string(k=8)
-    user_info = UserCreate(email=email, password=password, password_confirm=password)
-    user = create(db_session, user_info=user_info)
-    return user
-
-
 def test_create_access_token(client, db_session):
-    data = {"email": random_email()}
+    data = {"sub": random_email()}
     expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode = {"exp": expire, "sub": str(data)}
+    data.update({"exp": expire})
     encoded_jwt = jwt.encode(
-        to_encode, settings.SECRET_KEY, algorithm=settings.SECRET_ALGORITHM
+        data, settings.SECRET_KEY, algorithm=settings.SECRET_ALGORITHM
     )
     token = create_access_token(data)
     assert encoded_jwt == token
@@ -52,6 +45,13 @@ def test_create_user(client, db_session):
     assert user.email == email
     assert user.verified is False
     assert hasattr(user, "name")
+    assert hasattr(user, "profile_text")
+    assert hasattr(user, "profile_image_name")
+    assert hasattr(user, "profile_image_path")
+    assert hasattr(user, "verified")
+    assert hasattr(user, "verification_code")
+    assert hasattr(user, "created_at")
+    assert hasattr(user, "updated_at")
     assert verify_password(password, user.password) is True
 
     password_below_minimum = random_lower_string(k=7)
@@ -65,7 +65,8 @@ def test_create_user(client, db_session):
 
 
 def test_get_current_user(fake_user, db_session):
-    data = {"sub": fake_user.email}
+    user = fake_user.get("user")
+    data = {"sub": user.email}
     token = create_access_token(data)
     payload = jwt.decode(token, settings.SECRET_KEY, settings.SECRET_ALGORITHM)
     email = payload.get("sub")
@@ -74,9 +75,23 @@ def test_get_current_user(fake_user, db_session):
     user_02 = get_user(db_session, email=email)
 
     assert user_02
-    assert fake_user == user_01
-    assert fake_user == user_02
+    assert user == user_01
+    assert user == user_02
     assert user_01 == user_02
+
+
+def test_get_current_user_verified(fake_user, db_session):
+    user = fake_user.get("user")
+    info_to_be_updated = UserUpdate(verified=True)
+    update(db_session, user, info_to_be_updated)
+    verified_user = get_current_user_verified(user)
+    assert verified_user == user
+
+
+def test_get_current_user_not_verified(fake_user, db_session):
+    user = fake_user.get("user")
+    with pytest.raises(HTTPException):
+        get_current_user_verified(user)
 
 
 def test_get_user(client, db_session):
@@ -95,50 +110,52 @@ def test_get_user(client, db_session):
 
 
 def test_update_user_on_being_verified(fake_user, db_session):
-    old_verified = is_verified(fake_user)
+    user_01 = fake_user.get("user")
+    old_verified = is_verified(user_01)
 
     info_to_be_updated = UserUpdate(verified=True)
-    update(db_session, fake_user, info_to_be_updated)
-    user = get_user(db_session, email=fake_user.email)
+    update(db_session, user_01, info_to_be_updated)
+    user_02 = get_user(db_session, email=user_01.email)
 
-    assert user
-    assert jsonable_encoder(fake_user) == jsonable_encoder(user)
+    assert user_02
+    assert jsonable_encoder(user_01) == jsonable_encoder(user_02)
     assert old_verified is False
-    assert is_verified(user) is True
-    assert old_verified != is_verified(user)
+    assert is_verified(user_02) is True
+    assert old_verified != is_verified(user_02)
 
 
 def test_update_user_on_password(fake_user, db_session):
-    old_password = fake_user.password
+    user_01 = fake_user.get("user")
+    old_password = user_01.password
 
-    info_to_be_updated = UserUpdate(
-        password=get_password_hash(random_lower_string(k=8))
-    )
-    update(db_session, fake_user, info_to_be_updated)
+    info_to_be_updated = {"password": get_password_hash(random_lower_string(k=8))}
+    update(db_session, user_01, info_to_be_updated)
 
-    user = get_user(db_session, email=fake_user.email)
-    new_password = user.password
+    user_02 = get_user(db_session, email=user_01.email)
+    new_password = user_02.password
 
-    assert user
-    assert jsonable_encoder(fake_user) == jsonable_encoder(user)
+    assert user_02
+    assert jsonable_encoder(user_01) == jsonable_encoder(user_02)
     assert old_password != new_password
 
 
 def test_update_user_on_profile_text(fake_user, db_session):
-    old_profile_text = fake_user.profile_text
+    user_01 = fake_user.get("user")
+    old_profile_text = user_01.profile_text
 
     info_to_be_updated = UserUpdate(profile_text=f"{random_lower_string(k=10)}")
-    update(db_session, fake_user, info_to_be_updated)
+    update(db_session, user_01, info_to_be_updated)
 
-    user = get_user(db_session, email=fake_user.email)
-    new_profile_text = user.profile_text
+    user_02 = get_user(db_session, email=user_01.email)
+    new_profile_text = user_02.profile_text
 
-    assert user
-    assert jsonable_encoder(fake_user) == jsonable_encoder(user)
+    assert user_02
+    assert jsonable_encoder(user_01) == jsonable_encoder(user_02)
     assert old_profile_text != new_profile_text
 
 
 def test_delete_user(fake_user, db_session):
-    delete(db_session, fake_user)
-    user = get_user(db_session, email=fake_user.email)
+    user = fake_user.get("user")
+    delete(db_session, user)
+    user = get_user(db_session, email=user.email)
     assert not user
