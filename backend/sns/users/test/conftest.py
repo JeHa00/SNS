1a1185252989011aa5575baa4467a5bc9aecd55a
jeha00 <rudtls0611@naver.com>
2023-04-03 22:55:03 +0900

    CONF: add two functions to conftest.py - add fake_user - add get_user_token_headers_and_user_info

--- backend/sns/users/test/conftest.py
+++ backend/sns/users/test/conftest.py
@@ -1,11 +1,15 @@
 import pytest
-from typing import Generator, Any
+from typing import Generator, Any, Dict
 
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from sns.users.test.utils import random_lower_string, random_email
+from sns.users.schema import UserBase, UserCreate, UserUpdate
+from sns.users.service import create, update
 from sns.common.config import settings
 from sns.common.session import db
 from sns.common.base import Base
@@ -53,3 +57,36 @@ def client(app: FastAPI, db_session: TestingSessionLocal):
     app.dependency_overrides[db.get_db] = _get_test_db
     with TestClient(app) as client:
         yield client
+
+
+@pytest.fixture(scope="function")
+def fake_user(client, db_session):
+    password = random_lower_string(k=8)
+    user_info = UserCreate(
+        email=random_email(), password=password, password_confirm=password
+    )
+    user = create(db_session, user_info=user_info)
+    return {"user": user, "user_info": user_info}
+
+
+@pytest.fixture(scope="function")
+def get_user_token_headers_and_user_info(client, db_session) -> Dict[str, str]:
+    # fake_user 생성
+    email = random_email()
+    password = random_lower_string(k=8)
+    user_info = UserCreate(email=email, password=password, password_confirm=password)
+    fake_user = create(db_session, user_info=user_info)
+
+    # verified 업데이트
+    info_to_be_updated = UserUpdate(verified=True)
+    update(db_session, fake_user, info_to_be_updated)
+
+    # 로그인
+    login_info = UserBase(email=email, password=password)
+    data = jsonable_encoder(login_info)
+    response = client.post(f"{settings.API_V1_STR}/login", json=data)
+
+    # headers 반환
+    token = response.json().get("access_token")
+    headers = {"Authorization": f"Bearer {token}"}
+    return {"headers": headers, "user_info": data}
