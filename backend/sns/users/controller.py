f837d0fbd1719a6cd0716fa4d7648e55b66b1fe6
jeha00 <rudtls0611@naver.com>
2023-04-03 22:58:10 +0900

    REFACTOR: refactor controller about user domain

--- backend/sns/users/controller.py
+++ backend/sns/users/controller.py
@@ -8,13 +8,13 @@ from sns.common.session import db
 from sns.common.config import settings
 from sns.users.model import User
 from sns.users.schema import (
-    Msg,
-    Token,
-    UserBase,
+    UserPasswordUpdate,
     UserCreate,
     UserUpdate,
-    UserProfile,
     UserRead,
+    UserBase,
+    Token,
+    Msg,
 )
 from sns.users.service import (
     get_current_user_verified,
@@ -45,7 +45,6 @@ def signup(
 
     Args: \\
         - signup_info (schema.UserCreate) : 등록할 email과 password 정보
-        - db (Session, optional) : db session
 
     Returns: \\
         - 새로 생성한 User 객체를 반환한다.
@@ -63,9 +62,9 @@ def signup(
     user = get_user(db, email=signup_info.email)
     if user:
         if is_verified(user):
-            raise HTTPException(status_code=400, detail="이미 등록된 이메일입니다.")
+            raise HTTPException(status_code=403, detail="이미 인증된 이메일입니다.")
         else:
-            raise HTTPException(status_code=400, detail="인증 완료되지 못한 이메일입니다.")
+            raise HTTPException(status_code=403, detail="인증 완료되지 못한 이메일입니다.")
 
     # 비활성화 유저 생성
     new_user = create(db, signup_info)
@@ -79,7 +78,6 @@ def signup(
         url = f"http://0.0.0.0:8000{settings.API_V1_STR}/verification-email/{code}"
         data = {
             "email_to": signup_info.email,
-            "password": signup_info.password,
             "url": url,
         }
         background_tasks.add_task(send_new_account_email, **data)
@@ -92,7 +90,7 @@ def signup(
     return new_user
 
 
-@router.post(
+@router.patch(
     "/verification-email/{code}", response_model=Msg, status_code=status.HTTP_200_OK
 )
 def verify_email(code: str, db: Session = Depends(db.get_db)):
@@ -100,10 +98,9 @@ def verify_email(code: str, db: Session = Depends(db.get_db)):
 
     Args: \\
         - code (str) : url에 담겨진 code 정보
-        - db (Session, optional) : db session
 
     Returns: \\
-        - Msg : 계정 인증 완료
+        - Msg: 계정 인증 완료 메세지
     """
     user = db.query(User).filter(User.verification_code == code).first()
     if not user:
@@ -111,7 +108,7 @@ def verify_email(code: str, db: Session = Depends(db.get_db)):
             status_code=status.HTTP_403_FORBIDDEN, detail="등록되지 않은 인증 링크입니다."
         )
     update(db, user=user, user_info={"verified": True})
-    return {"status": "success", "msg": "이메일 등록이 완료되었습니다."}
+    return {"status": "success", "msg": "이메일 인증이 완료되었습니다."}
 
 
 @router.post("/login", response_model=Token, status_code=status.HTTP_200_OK)
@@ -120,7 +117,6 @@ def login(login_info: UserBase, db: Session = Depends(db.get_db)):
 
     Args: \\
         - login_info (schema.UserBase): 로그인 시 입력한 email과 password를 의미한다.
-        - db (Session, optional): db session
 
     Returns: \\
         - dict: 입력한 정보가 정확하면 access token을 발행한다.
@@ -128,53 +124,54 @@ def login(login_info: UserBase, db: Session = Depends(db.get_db)):
     user = get_user(db, email=login_info.email, password=login_info.password)
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_400_BAD_REQUEST,
             detail="이메일 또는 비밀번호 정보가 정확하지 않습니다.",
+            headers={"WWW-Authenticate": "Bearer"},
         )
 
     if not is_verified(user):
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
+            status_code=status.HTTP_403_FORBIDDEN,
             detail="먼저 이메일 인증을 완료하세요.",
         )
-    access_token = create_access_token(data={"email": login_info.email})
+    access_token = create_access_token(data={"sub": login_info.email})
     return {"access_token": access_token, "token_type": "Bearer"}
 
 
-@router.post("/password-reset", response_model=Msg, status_code=status.HTTP_200_OK)
+@router.patch("/password-reset", response_model=Msg, status_code=status.HTTP_200_OK)
 def reset_password(
-    email_to: str, background_tasks: BackgroundTasks, db: Session = Depends(db.get_db)
+    user_info: UserBase,
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(db.get_db),
 ):
     """비밀번호를 초기화합니다.
 
     Args: \\
-        - email_to (str): 임시 비밀번호를 발급 받을 이메일
-        - db (Session, optional): db session
+        - user_info (schema.UserBase): 임시 비밀번호를 발급 받을 이메일
 
     Returns: \\
-        - Msg: 비밀번호 초기화 이메일 송신 완료
+        - Msg: 비밀번호 초기화 이메일 송신 완료 메세지
     """
-    user = get_user(db, email=email_to)
+    user = get_user(db, email=user_info.email)
     if user:
         if is_verified(user):
             try:
                 temporary_password = secrets.token_urlsafe(8)
                 update(db, user, {"password": get_password_hash(temporary_password)})
-                data = {"email_to": email_to, "password": temporary_password}
+                data = {"email_to": user_info.email, "password": temporary_password}
                 background_tasks.add_task(send_reset_password_email, **data)
                 return {"status": "success", "msg": "비밀번호 초기화를 위한 이메일 송신이 완료되었습니다."}
             except Exception:
                 raise
         else:
-            raise HTTPException(status_code=400, detail="인증 완료되지 못한 이메일입니다.")
+            raise HTTPException(status_code=403, detail="먼저 이메일 인증을 완료하세요.")
     else:
-        raise HTTPException(status_code=400, detail="등록된 회원이 아닙니다.")
+        raise HTTPException(status_code=403, detail="등록된 회원이 아닙니다.")
 
 
-@router.post("/password-change", response_model=Msg, status_code=status.HTTP_200_OK)
+@router.patch("/password-change", response_model=Msg, status_code=status.HTTP_200_OK)
 def change_password(
-    current_password: str,
-    new_password: str,
+    password_info: UserPasswordUpdate,
     current_user: UserBase = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db),
 ):
@@ -183,19 +180,21 @@ def change_password(
        일치하지 않으면 변경하지 않습니다.
 
     Args: \\
-        - current_password (str): 현재 패스워드
-        - new_password (str): 새로운 패스워드
-        - current_user (User, optional): 현재 유저 정보
-        - db (Session, optional): db session
+        password_info (UserPasswordUpdate): 현재 패스워드와 새 패스워드 정보
+        current_user (UserBase, optional): 현재 유저 정보
 
     Returns: \\
-        - Msg: 실행 완료
+        Msg: 실행 완료 메세지
     """
+    current_password, new_password = (
+        password_info.current_password,
+        password_info.new_password,
+    )
+
     if verify_password(current_password, current_user.password):
-        hashed_new_password = {"password": get_password_hash(new_password)}
         try:
             user = get_user(db, email=current_user.email)
-            update(db, user, hashed_new_password)
+            update(db, user, {"password": get_password_hash(new_password)})
             return {"status": "success", "msg": "비밀번호가 변경되었습니다."}
         except Exception:
             raise HTTPException(status_code=500, detail="비밀번호 변경에 실패했습니다.")
@@ -205,7 +204,7 @@ def change_password(
 
 @router.get(
     "/users/{user_id}",
-    response_model=UserRead | UserProfile,
+    response_model=UserRead,
     status_code=status.HTTP_200_OK,
 )
 def read_user(
@@ -217,7 +216,7 @@ def read_user(
 
     Args: \\
         - user_id (int): db에 저장된 user id
-        - db (Session, optional): db session
+        - current_user (UserBase, optional): 현재 유저 정보
 
     Returns: \\
         - 유저 정보
@@ -239,7 +238,7 @@ def read_user(
 @router.patch("/users/{user_id}", response_model=Msg, status_code=status.HTTP_200_OK)
 def update_user(
     user_id: int,
-    info_to_update: UserUpdate,
+    info_to_be_updated: UserUpdate,
     current_user: UserBase = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db),
 ):
@@ -247,18 +246,17 @@ def update_user(
 
     Args: \\
         - user_id (int): db에 저장된 user id
-        - info_to_update (UserUpdate): 업데이트할 user 정보
-        - current_user (User, optional): token에서 가져온 현재 유저 정보
-        - db (Session, optional): db session
+        - info_to_be_updated (UserUpdate): 업데이트할 user 정보
+        - current_user (UserBase): token에서 가져온 현재 유저 정보
 
     Returns: \\
-        - Msg: 실행 완료
+        - Msg: 실행 완료 메세지
     """
     selected_user = db.query(User).filter(User.id == user_id).first()
     if selected_user.email == current_user.email:
         try:
             user_to_update = get_user(db, email=current_user.email)
-            update(db, user_to_update, info_to_update)
+            update(db, user_to_update, info_to_be_updated)
             return {"status": "success", "msg": "계정 정보가 변경되었습니다."}
         except Exception:
             raise HTTPException(
@@ -282,10 +280,9 @@ def delete_user(
     Args: \\
         - user_id (int): db에 저장된 user id
         - current_user (User, optional): token에서 가져온 현재 유저 정보
-        - db (Session, optional): db session
 
     Returns: \\
-        - Msg: 계정 삭제 완료
+        - Msg: 계정 삭제 완료 메세지
     """
     selected_user = db.query(User).filter(User.id == user_id).first()
     if selected_user.email == current_user.email:
