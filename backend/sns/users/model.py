57ee15d4acca4cb506dde39efb8ec8b2e5529276
jeha00 <rudtls0611@naver.com>
2023-04-04 18:48:48 +0900

    REFACTOR: refactor verified attribute of User domain

--- backend/sns/users/model.py
+++ backend/sns/users/model.py
@@ -1,6 +1,8 @@
 from sqlalchemy import Column, String, Boolean
+from sqlalchemy.orm import relationship
 
 from sns.common.base import Base, BaseMixin
+from sns.posts.model import Post
 
 
 class User(Base, BaseMixin):
@@ -10,5 +12,5 @@ class User(Base, BaseMixin):
     profile_text = Column(String(300), nullable=True)
     profile_image_name = Column(String(50), nullable=True)
     profile_image_path = Column(String(200), nullable=True)
-    verified = Column(Boolean, nullable=False, default=False)
+    verified = Column(Boolean, default=False)
     verification_code = Column(String(20), nullable=True, unique=True)
