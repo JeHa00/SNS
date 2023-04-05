742bd5448e670b15552d4bbe2d150c5770e94e0e
jeha00 <rudtls0611@naver.com>
2023-04-05 12:25:03 +0900

    CONF: add conftest.py on Post domain test

--- backend/sns/posts/test/conftest.py
+++ backend/sns/posts/test/conftest.py
@@ -1,3 +1,36 @@
+from typing import List, Dict
+import random
+import string
+
 import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
+
 
 from sns.common.conftest import app, db_session, client
+from sns.users.test.utils import random_lower_string
+from sns.users.test.conftest import fake_user
+from sns.posts.schema import PostCreate
+from sns.posts.service import create
+from sns.posts.model import Post
+
+
+@pytest.fixture(scope="function")
+def fake_post(client: TestClient, db_session: Session, fake_user: Dict) -> Post:
+    content = "".join(random.choices(string.ascii_lowercase, k=500))
+    post_info = PostCreate(content=content)
+    user = fake_user.get("user")
+    post = create(db_session, post_info=post_info, writer_id=user.id)
+    return post
+
+
+@pytest.fixture(scope="function")
+def fake_multi_posts(client: TestClient, db_session: Session, fake_user: Dict) -> List[Post]:
+    post_total_count_to_make = 100
+
+    while post_total_count_to_make > 0:
+        content = "".join(random.choices(string.ascii_lowercase, k=1000))
+        post_info = PostCreate(content=content)
+        user = fake_user.get("user")
+        create(db_session, post_info=post_info, writer_id=user.id)
+        post_total_count_to_make -= 1
