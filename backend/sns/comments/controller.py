a37a6c35a663f2ef1be57041f8504e6dd262c41b
jeha00 <rudtls0611@naver.com>
2023-04-06 19:17:07 +0900

    FEAT: add schema to controller.py on Comment domain

--- backend/sns/comments/controller.py
+++ backend/sns/comments/controller.py
@@ -1,16 +1,19 @@
-from fastapi import APIRouter, Depends, status, HTTPException, Body
+from typing import List
+
+from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 
 from sns.common.session import db
 from sns.users.service import get_current_user_verified
 from sns.users.model import User
 from sns.users.schema import Msg
+from sns.comments.schema import Comment, CommentCreate, CommentUpdate
 
 
 router = APIRouter()
 
 
-@router.get("/posts/{post_id}/comments", response_model='', status_code=status.HTTP_200_OK)
+@router.get("/posts/{post_id}/comments", response_model=List[Comment], status_code=status.HTTP_200_OK)
 def get_comments_on_a_post(
     post_id: int,
     db: Session = Depends(db.get_db)
@@ -18,7 +21,7 @@ def get_comments_on_a_post(
     pass
 
 
-@router.get("/users/{user_id}/comments", response_model='', status_code=status.HTTP_200_OK)
+@router.get("/users/{user_id}/comments", response_model=List[Comment], status_code=status.HTTP_200_OK)
 def get_comments_of_a_user(
     post_id: int, 
     db: Session = Depends(db.get_db)
@@ -26,7 +29,7 @@ def get_comments_of_a_user(
     pass
 
 
-@router.get("/posts/{post_id}/comments/{comment_id}", response_model='', status_code=status.HTTP_200_OK)
+@router.get("/posts/{post_id}/comments/{comment_id}", response_model=Comment, status_code=status.HTTP_200_OK)
 def get_comment(
     post_id: int,
     comment_id: int,
@@ -35,19 +38,21 @@ def get_comment(
     pass
 
 
-@router.post("/posts/{post_id}/comments", response_model='', status_code=status.HTTP_201_CREATED)
+@router.post("/posts/{post_id}/comments", response_model=Comment, status_code=status.HTTP_201_CREATED)
 def create_comment(
     post_id: int,
+    data_to_be_created: CommentCreate,
     current_user: User = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db)
 ):
     pass
 
 
-@router.put("/posts/{post_id}/comments/{comment_id}", response_model='', status_code=status.HTTP_200_OK)
+@router.put("/posts/{post_id}/comments/{comment_id}", response_model=Comment, status_code=status.HTTP_200_OK)
 def update_comment(
     post_id: int,
     comment_id: int,
+    data_to_be_updated: CommentUpdate, 
     current_user: User = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db)
 ):
