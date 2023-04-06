b77414d4ec598ba69a336197301f49d6117542dd
jeha00 <rudtls0611@naver.com>
2023-04-06 19:48:05 +0900

    CONF: delete unused library and change function

--- backend/sns/posts/test/conftest.py
+++ backend/sns/posts/test/conftest.py
@@ -1,11 +1,8 @@
-from typing import List, Dict
-import random
-import string
+from typing import Dict
 
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
-
 
 from sns.common.conftest import app, db_session, client
 from sns.users.test.utils import random_lower_string
@@ -17,7 +14,7 @@ from sns.posts.model import Post
 
 @pytest.fixture(scope="function")
 def fake_post(client: TestClient, db_session: Session, fake_user: Dict) -> Post:
-    content = "".join(random.choices(string.ascii_lowercase, k=500))
+    content = random_lower_string(k=1000)
     post_info = PostCreate(content=content)
     user = fake_user.get("user")
     post = create(db_session, post_info=post_info, writer_id=user.id)
@@ -29,7 +26,7 @@ def fake_multi_posts(client: TestClient, db_session: Session, fake_user: Dict) -
     post_total_count_to_make = 100
 
     while post_total_count_to_make > 0:
-        content = "".join(random.choices(string.ascii_lowercase, k=1000))
+        content = random_lower_string(k=1000)
         post_info = PostCreate(content=content)
         user = fake_user.get("user")
         create(db_session, post_info=post_info, writer_id=user.id)
