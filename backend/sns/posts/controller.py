d93aa7cb09addb5ae8a017d50ff465fbe70c567d
jeha00 <rudtls0611@naver.com>
2023-04-06 12:40:36 +0900

    FEAT: add create_post and update_post api - edit docs of other apis

--- backend/sns/posts/controller.py
+++ backend/sns/posts/controller.py
@@ -1,14 +1,13 @@
 from typing import List
 
 from fastapi import APIRouter, Depends, status, HTTPException
-from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from sns.common.session import db
-from sns.users.service import get_current_user_verified, get_user
-from sns.users.schema import Msg, UserBase
+from sns.users.service import get_current_user_verified
+from sns.users.schema import Msg
 from sns.users.model import User
-from sns.posts.schema import Post
+from sns.posts.schema import Post, PostCreate, PostUpdate
 from sns.posts import model
 from sns.posts.service import (
     get_multi_posts,
@@ -33,14 +32,12 @@ def read_post(
         post_id (int): 읽어올 post의 id
 
     Returns:
-        model.Post: post model 객체를 반환
+        Post: post_id에 해당되는 post 반환  
     """
-
-    # post.id가 post_id인 post 정보를 읽어온다.
     post = get_post(db, post_id=post_id)    
     
     if not post:
-        raise HTTPException(status_code=404, detail="해당 id의 포스트를 찾을 수 없습니다.")        
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="해당 id의 포스트를 찾을 수 없습니다.")        
     else:
         return post
 
@@ -50,56 +47,114 @@ def read_posts(
     user_id: int,
     db: Session = Depends(db.get_db)
 ):
-    # user.id가 user_id인 writer의 post들을 모두 가져온다.
+    """user_id에 일치하는 user가 작성한 post들을 조회
+
+    Args:    
+        user_id (int): user의 id
+
+    Returns:  
+        List[Post]: 여러 post가 list 배열에 담겨져 반환  
+    """
     selected_user = db.query(User).filter(User.id == user_id).first()
     if selected_user:
         posts = get_multi_posts(db, writer_id=user_id)
         if len(posts) == 0:
-            raise HTTPException(status_code=404, detail="생성된 글이 없습니다.")
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="작성된 글이 없습니다.")
         else:
             return posts
     else:
-        raise HTTPException(status_code=404, detail="등록되지 않은 유저입니다.")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="등록된 회원이 아닙니다.")
 
-@router.post("/users/{user_id}/posts/{post_id}", response_model=Post, status_code=status.HTTP_201_CREATED)
+
+@router.post("/users/{user_id}/posts", response_model=Post, status_code=status.HTTP_201_CREATED)
 def create_post(
-    content: str, 
-    current_user: UserBase = Depends(get_current_user_verified),
+    user_id: int, 
+    data_to_be_created: PostCreate,
+    current_user: User = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db)
 ):
-    # 현재 로그인되어 있는 유저가 글을 추가한다.
-    pass 
+    """user_id가 현재 로그인된 user와 동일할 때 post를 생성한다.
+
+    Args:
+        user_id (int): 글을 작성할 user의 id
+        data_to_be_created (PostCreate): 생성할 post의 content 정보
+        current_user (User, optional): 현재 로그인된 user 정보
+
+    Returns:
+       Post: 생성된 post 정보 반환
+    """
+    selected_user = db.query(User).filter(User.id == user_id).first()
+    if selected_user:
+        if user_id == current_user.id:
+            post = create(db, post_info=data_to_be_created, writer_id=user_id)
+            return post
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="작성할 권한이 없습니다."
+            )
+    else: 
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="등록된 회원이 아닙니다.")
 
 
 @router.put("/users/{user_id}/posts/{post_id}", response_model=Post, status_code=status.HTTP_200_OK)
 def update_post(
     user_id: int,
     post_id: int,
-    current_user: UserBase = Depends(get_current_user_verified),
+    data_to_be_updated: PostUpdate, 
+    current_user: User = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db)
 ):
-    # post.id가 post_id인 post를 편집한다. 이 때 current_user일 때만 가능하다.
-    pass 
+    """user_id가 현재 user id와 동일하여 수정 권한이 있을 때 post_id에 해당되는 post를 수정한다.
+
+    Args:
+        user_id (int): 수정할 user의 id
+        post_id (int): 수정될 post의 id
+        data_to_be_updated (PostUpdate): _description_
+        current_user (User, optional): _description_. Defaults to Depends(get_current_user_verified).
+        db (Session, optional): _description_. Defaults to Depends(db.get_db).
+
+    Raises:
+        HTTPException: _description_
+        HTTPException: _description_
+        HTTPException: _description_
+
+    Returns:
+        Post: 수정된 post 정보 반환
+    """
+    selected_user = db.query(User).filter(User.id == user_id).first()
+    if selected_user:
+        if selected_user.id == current_user.id:
+            post = get_post(db, post_id=post_id)
+            if not post:
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="해당되는 글이 없습니다.")
+            else:
+                update(db, post_info=post, data_to_be_updated=data_to_be_updated)
+                return post
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="작성할 권한이 없습니다."
+            )
+    else:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="등록된 회원이 아닙니다.")
 
 
 @router.delete("/users/{user_id}/posts/{post_id}", response_model=Msg, status_code=status.HTTP_200_OK)
 def delete_post(
     user_id: int, 
     post_id: int,
-    current_user: UserBase = Depends(get_current_user_verified),
+    current_user: User = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db)
-) -> dict:
+):
     """user_id가 current_user의 id와 동일할 때, 해당 post_id를 가진 post를 삭제한다.
 
-    Args:
-        user_id (int): user의 id
-        post_id (int): post의 id
-        current_user (UserBase): 현재 로그인된 유저
-
-    Returns:
-        schema.Msg: 삭제 성공 메세지를 반환
-    """  
-
+    Args:  
+        user_id (int): 삭제시킬 user의 id  
+        post_id (int): 삭제될 post의 id  
+        current_user (User): 현재 로그인된 user 정보
+  
+    Returns:  
+        schema.Msg: 삭제 성공 메세지를 반환  
+    """    
     if user_id == current_user.id:
         remove(db, post_info=post_id)
         return {"status": "success", "msg": "글이 삭제되었습니다."}
