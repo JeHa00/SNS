55093d692df2ac7fa1f4aefe5f70e3c41cce2c4d
jeha00 <rudtls0611@naver.com>
2023-04-07 19:39:22 +0900

    CONF: add __repr__ to Post model class

--- backend/sns/posts/model.py
+++ backend/sns/posts/model.py
@@ -8,3 +8,7 @@ class Post(Base, BaseMixin):
     content = Column(String(1000), nullable=False)
     writer_id = Column(Integer, ForeignKey("user.id"))
     writer = relationship("User", back_populates="posts")
+
+    def __repr__(self) -> str:
+        return f"Post(id={self.id}, writer_id={self.writer_id}, created_at={self.created_at})"
+    
