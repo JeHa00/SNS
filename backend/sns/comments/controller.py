26fd7f0a34fff1e88109b69833f883cd95c97e1a
jeha00 <rudtls0611@naver.com>
2023-04-10 19:39:14 +0900

    FEAT: add apis on Comment domain

--- backend/sns/comments/controller.py
+++ backend/sns/comments/controller.py
@@ -7,63 +7,132 @@ from sns.common.session import db
 from sns.users.service import get_current_user_verified
 from sns.users.model import User
 from sns.users.schema import Msg
+from sns.posts.service import get_post
 from sns.comments.schema import Comment, CommentCreate, CommentUpdate
+from sns.comments.repository import comment_crud
 
 
 router = APIRouter()
 
 
-@router.get("/posts/{post_id}/comments", response_model=List[Comment], status_code=status.HTTP_200_OK)
-def get_comments_on_a_post(
-    post_id: int,
-    db: Session = Depends(db.get_db)
-):
-    pass
+@router.get(
+    "/posts/{post_id}/comments",
+    response_model=List[Comment],
+    status_code=status.HTTP_200_OK,
+)
+def get_comments_on_a_post(post_id: int, db: Session = Depends(db.get_db)):
+    selected_post = get_post(db, post_id=post_id)
+    if selected_post:
+        comments = comment_crud.get_multi_comments(db, post_id=post_id)
+        return comments
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="해당 id의 포스트를 찾을 수 없습니다."
+        )
 
 
-@router.get("/users/{user_id}/comments", response_model=List[Comment], status_code=status.HTTP_200_OK)
-def get_comments_of_a_user(
-    post_id: int, 
-    db: Session = Depends(db.get_db)
-):
-    pass
+@router.get(
+    "/users/{user_id}/comments",
+    response_model=List[Comment],
+    status_code=status.HTTP_200_OK,
+)
+def get_comments_of_a_user(user_id: int, db: Session = Depends(db.get_db)):
+    selected_user = db.query(User).filter(User.id == user_id).first()
+    if selected_user:
+        comments = comment_crud.get_multi_comments(db, writer_id=user_id)
+        if len(comments) == 0:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND, detail="작성된 댓글이 없습니다."
+            )
+        else:
+            return comments
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="등록된 회원이 아닙니다."
+        )
 
 
-@router.get("/posts/{post_id}/comments/{comment_id}", response_model=Comment, status_code=status.HTTP_200_OK)
-def get_comment(
-    post_id: int,
-    comment_id: int,
-    db: Session = Depends(db.get_db)
-):
-    pass
-
-
-@router.post("/posts/{post_id}/comments", response_model=Comment, status_code=status.HTTP_201_CREATED)
+@router.post(
+    "/users/{user_id}/posts/{post_id}/comments",
+    response_model=Comment,
+    status_code=status.HTTP_201_CREATED,
+)
 def create_comment(
+    user_id: int,
     post_id: int,
     data_to_be_created: CommentCreate,
     current_user: User = Depends(get_current_user_verified),
-    db: Session = Depends(db.get_db)
+    db: Session = Depends(db.get_db),
 ):
-    pass
+    selected_user = db.query(User).filter(User.id == user_id).first()
+    if selected_user:
+        if selected_user.id == current_user.id:
+            content = CommentCreate(content=data_to_be_created)
+            comment = comment_crud.create(
+                db, data_to_be_created=content, writer_id=user_id, post_id=post_id
+            )
+            return comment
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="작성할 권한이 없습니다."
+            )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="등록된 회원이 아닙니다."
+        )
 
 
-@router.put("/posts/{post_id}/comments/{comment_id}", response_model=Comment, status_code=status.HTTP_200_OK)
+@router.put(
+    "/users/{user_id}/comments/{comment_id}",
+    response_model=Comment,
+    status_code=status.HTTP_200_OK,
+)
 def update_comment(
-    post_id: int,
+    user_id: int,
     comment_id: int,
-    data_to_be_updated: CommentUpdate, 
+    data_to_be_updated: CommentUpdate,
     current_user: User = Depends(get_current_user_verified),
-    db: Session = Depends(db.get_db)
+    db: Session = Depends(db.get_db),
 ):
-    pass
+    selected_user = db.query(User).filter(User.id == user_id).first()
+    if selected_user:
+        if selected_user.id == current_user.id:
+            content = CommentUpdate(content=data_to_be_updated)
+            comment = comment_crud.update(
+                db, comment_info=comment_id, data_to_be_updated=content
+            )
+            return comment
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="수정할 권한이 없습니다."
+            )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="등록된 회원이 아닙니다."
+        )
 
 
-@router.delete("/posts/{post_id}/comments/{comment_id}", response_model=Msg, status_code=status.HTTP_200_OK)
+@router.delete(
+    "/users/{user_id}/comments/{comment_id}",
+    response_model=Msg,
+    status_code=status.HTTP_200_OK,
+)
 def delete_comment(
-    post_id: int,
+    user_id: int,
     comment_id: int,
     current_user: User = Depends(get_current_user_verified),
-    db: Session = Depends(db.get_db)
+    db: Session = Depends(db.get_db),
 ):
-    pass
+    selected_user = db.query(User).filter(User.id == user_id).first()
+    if selected_user:
+        if selected_user.id == current_user.id:
+            comment_crud.remove(db, comment_info=comment_id)
+            return {"status": "success", "msg": "댓글이 삭제되었습니다."}
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="삭제할 권한이 없습니다."
+            )
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="등록된 회원이 아닙니다."
+        )
