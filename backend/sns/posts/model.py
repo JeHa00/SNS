5bf3618ac70aa083399fffa946e9dcd97b21606a
jeha00 <rudtls0611@naver.com>
2023-04-11 20:54:19 +0900

    FIX: fix relationship between Post and Comment domain model

--- backend/sns/posts/model.py
+++ backend/sns/posts/model.py
@@ -2,12 +2,10 @@ from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 
 from sns.common.base import Base, BaseMixin
-from sns.comments.model import Comment
 
 
 class Post(Base, BaseMixin):
     content = Column(String(1000), nullable=False)
     writer_id = Column(Integer, ForeignKey("user.id"))
-    comment_id = Column(Integer, ForeignKey("comment.id"))
     writer = relationship("User", back_populates="posts")
     comments = relationship("Comment", back_populates="post")
