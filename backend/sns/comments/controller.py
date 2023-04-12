2cd7fac247f4cb569c4b2477d8b820a7caa571d6
jeha00 <rudtls0611@naver.com>
2023-04-12 20:40:40 +0900

    DOCS: add annotations to controller.py on Comment domain

--- backend/sns/comments/controller.py
+++ backend/sns/comments/controller.py
@@ -10,6 +10,7 @@ from sns.users.schema import Msg
 from sns.posts.service import get_post
 from sns.comments.schema import Comment, CommentCreate, CommentUpdate
 from sns.comments.repository import comment_crud
+from sns.comments import model
 
 
 router = APIRouter()
@@ -20,7 +21,20 @@ router = APIRouter()
     response_model=List[Comment],
     status_code=status.HTTP_200_OK,
 )
-def get_comments_on_a_post(post_id: int, db: Session = Depends(db.get_db)):
+def get_comments_on_a_post(
+    post_id: int, db: Session = Depends(db.get_db)
+) -> List[model.Comment]:
+    """**한 개의 post에 대한 모든 comment를 조회한다.**
+
+    **Args:**
+        - post_id (int): comment가 달린 post의 id
+
+    **Raises:**
+        - HTTPException(404 NOT FOUND): id에 해당되는 post가 없을 때 발생하는 에러
+
+    **Returns:**
+        - List[model.Comment]: 해당되는 여러 comment를 list에 담겨 반환
+    """
     selected_post = get_post(db, post_id=post_id)
     if selected_post:
         comments = comment_crud.get_multi_comments(db, post_id=post_id)
@@ -36,7 +50,21 @@ def get_comments_on_a_post(post_id: int, db: Session = Depends(db.get_db)):
     response_model=List[Comment],
     status_code=status.HTTP_200_OK,
 )
-def get_comments_of_a_user(user_id: int, db: Session = Depends(db.get_db)):
+def get_comments_of_a_user(
+    user_id: int, db: Session = Depends(db.get_db)
+) -> List[Comment]:
+    """**한 user가 작성한 모든 comment를 조회한다.**
+
+    **Args:**
+        - user_id (int): comment를 작성한 user의 id
+
+    **Raises:**
+        - HTTPException(404 NOT FOUND): user_id에 해당되는 user가 작성한 댓글이 없으면 발생하는 에러
+        - HTTPException(403 FORBIDDEN): user_id로 등록된 회원이 없으면 발생하는 에러
+
+    **Returns:**
+        - List[model.Comment]: 해당되는 여러 comment를 list에 담겨 반환
+    """
     selected_user = db.query(User).filter(User.id == user_id).first()
     if selected_user:
         comments = comment_crud.get_multi_comments(db, writer_id=user_id)
@@ -63,7 +91,22 @@ def create_comment(
     data_to_be_created: CommentCreate,
     current_user: User = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db),
-):
+) -> Comment:
+    """**전달된 정보로 comment를 생성한다.**
+
+    **Args:**
+        - user_id (int): comment를 생성할 user의 id
+        - post_id (int): comment가 작성될 post의 id
+        - data_to_be_created (CommentCreate): 작성될 comment의 내용
+        - current_user (User, optional): 현재 로그인된 user의 정보
+
+    **Raises:**
+        - HTTPException(401 UNAUTHORIZED): user_id와 current_user의 id가 다를 경우 발생하는 에러
+        - HTTPException(403 FORBIDDEN): user_id로 등록된 회원이 없으면 발생하는 에러
+
+    **Returns:**
+        - Comment: 생성된 comment 정보를 반환
+    """
     selected_user = db.query(User).filter(User.id == user_id).first()
     if selected_user:
         if selected_user.id == current_user.id:
@@ -95,7 +138,22 @@ def update_comment(
     data_to_be_updated: CommentUpdate,
     current_user: User = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db),
-):
+) -> Comment:
+    """**전달된 정보로 comment를 수정한다.**
+
+    **Args:**
+        - user_id (int): comment를 작성했던 user의 id
+        - comment_id (int): 수정할 comment의 id
+        - data_to_be_updated (CommentUpdate): 변경될 내용
+        - current_user (User, optional): 현재 로그인된 user의 정보
+
+    **Raises:**
+        - HTTPException(401 UNAUTHORIZED): user_id와 current_user의 id가 다를 경우 발생하는 에러
+        - HTTPException(403 FORBIDDEN): user_id로 등록된 회원이 없으면 발생하는 에러
+
+    **Returns:**
+        - Comment: 수정된 comment 정보를 반환
+    """
     selected_user = db.query(User).filter(User.id == user_id).first()
     if selected_user:
         if selected_user.id == current_user.id:
@@ -123,7 +181,21 @@ def delete_comment(
     comment_id: int,
     current_user: User = Depends(get_current_user_verified),
     db: Session = Depends(db.get_db),
-):
+) -> Msg:
+    """**전달된 정보에 해당되는 comment를 삭제한다.**
+
+    **Args:**
+        - user_id (int): comment를 작성했던 user의 id
+        - comment_id (int): 삭제할 comment의 id
+        - current_user (User, optional): 현재 로그인된 user의 정보
+
+    **Raises:**
+        - HTTPException(401 UNAUTHORIZED): user_id와 current_user의 id가 다를 경우 발생하는 에러
+        - HTTPException(403 FORBIDDEN): user_id로 등록된 회원이 없으면 발생하는 에러
+
+    **Returns:**
+        - schema.Msg: 삭제 성공 메세지를 반환
+    """
     selected_user = db.query(User).filter(User.id == user_id).first()
     if selected_user:
         if selected_user.id == current_user.id:
