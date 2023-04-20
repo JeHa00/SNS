9e663a469badd61d8ca1086a1376618055c4ea13
jeha00 <rudtls0611@naver.com>
2023-04-20 23:58:54 +0900

    FEAT: add a attribute liker to User model for PostLike model

--- backend/sns/users/model.py
+++ backend/sns/users/model.py
@@ -13,4 +13,8 @@ class User(Base, BaseMixin):
     profile_image_path = Column(String(200), nullable=True)
     verified = Column(Boolean, default=False)
     verification_code = Column(String(20), nullable=True, unique=True)
+
     posts = relationship("Post", back_populates="writer", cascade="all, delete-orphan")
+    liker = relationship(
+        "PostLike", back_populates="who_like", cascade="all, delete-orphan"
+    )
