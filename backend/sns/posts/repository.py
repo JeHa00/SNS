558e69fc267b866e6eb53dba57bb6b0374cf8e46
jeha00 <rudtls0611@naver.com>
2023-04-21 00:00:08 +0900

    FEAT: add crud functions about PostLike model

--- backend/sns/posts/repository.py
+++ backend/sns/posts/repository.py
@@ -3,8 +3,9 @@ from typing import List
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
-from sns.posts.model import Post
-from sns.posts.schema import PostCreate, PostUpdate
+from sns.users.model import User
+from sns.posts.model import Post, PostLike
+from sns.posts import schema
 
 
 class PostDB:
@@ -41,7 +42,7 @@ class PostDB:
             .all()
         )
 
-    def create(self, db: Session, post_info: PostCreate, writer_id: int) -> Post:
+    def create(self, db: Session, post_info: schema.PostCreate, writer_id: int) -> Post:
         """주어진 post_info, writer_id 정보를 가지는 post를 생성한다.
 
         Args:
@@ -62,7 +63,7 @@ class PostDB:
         return db_obj
 
     def update(
-        self, db: Session, post_info: Post | int, data_to_be_updated: PostUpdate
+        self, db: Session, post_info: Post | int, data_to_be_updated: schema.PostUpdate
     ) -> Post:
         """post_info에 해당되는 Post 객체를 data_to_be_updated 정보를 가지도록 수정한다.
 
@@ -109,4 +110,77 @@ class PostDB:
         db.commit()
 
 
+class PostLikeDB:
+    # liker 조회
+    def get_users_who_like(self, db: Session, like_target_id: int) -> List[User]:
+        """like_target_id에 일치하는 post를 좋아요한 유저들을 조회한다.
+
+        Args:
+            like_target_id (int): like를 받은 post의 id
+
+        Returns:
+            _type_: _description_
+        """
+        # like_target_id에 해당되는 post에 좋아요를 누른 다수의 user 조회
+        return (
+            db.query(PostLike)
+            .filter(PostLike.like_target_id == like_target_id, PostLike.is_like is True)
+            .all()
+        )
+
+    # likee 조회
+    def get_like_targets(self, db: Session, who_like_id: int):
+        # who_like_id에 해당되는 user가 좋아요를 누른 다수의 post 조회
+        # return db.query(PostLike).filter(PostLike.who_like_id == who_like_id).all()
+        return (
+            db.query(PostLike)
+            .filter(PostLike.who_like_id == who_like_id, PostLike.is_like is True)
+            .all()
+        )
+
+    def like(self, db: Session, like_info: schema.PostLike):
+        is_like, who_like_id, like_target_id = like_info.values()
+
+        db_obj = (
+            db.query(PostLike)
+            .filter(
+                PostLike.who_like_id == who_like_id,
+                PostLike.like_target_id == like_target_id,
+            )
+            .first()
+        )
+
+        if db_obj is None:
+            db_obj = PostLike(**like_info)
+        else:
+            setattr(db_obj, "is_like", is_like)
+
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+
+        return db_obj
+
+    def unlike(self, db: Session, unlike_info: schema.PostUnlike):
+        is_like, who_like_id, like_target_id = unlike_info.values()
+
+        db_obj = (
+            db.query(PostLike)
+            .filter(
+                PostLike.who_like_id == who_like_id,
+                PostLike.like_target_id == like_target_id,
+            )
+            .first()
+        )
+
+        setattr(db_obj, "is_like", is_like)
+
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+
+        return db_obj
+
+
 post_crud = PostDB()
+post_like_crud = PostLikeDB()
