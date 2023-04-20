670127c6ccfb7c27f19c45d11f364de068fc9f55
jeha00 <rudtls0611@naver.com>
2023-04-20 23:53:52 +0900

    FEAT: add PostLike model to Post domain

--- backend/sns/posts/model.py
+++ backend/sns/posts/model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, Boolean, ForeignKey
 from sqlalchemy.orm import relationship
 
 from sns.common.base import Base, BaseMixin
@@ -9,5 +9,26 @@ class Post(Base, BaseMixin):
     writer_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
     writer = relationship("User", back_populates="posts")
 
+    likee = relationship(
+        "PostLike", back_populates="like_target", cascade="all, delete-orphan"
+    )
+
     def __repr__(self) -> str:
         return f"Post(id={self.id}, writer_id={self.writer_id})"
+
+
+class PostLike(Base, BaseMixin):
+    who_like_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
+    who_like = relationship("User", back_populates="liker")
+
+    like_target_id = Column(Integer, ForeignKey("post.id", ondelete="CASCADE"))
+    like_target = relationship("Post", back_populates="likee")
+
+    is_like = Column(Boolean, default=True)
+
+    def __repr__(self) -> str:
+        return (
+            f"PostLike(who_like_id={self.who_like_id}, "
+            f"like_target_id={self.like_target_id}, "
+            f"is_like={self.is_like})"
+        )
