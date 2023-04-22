e7ceb2dd8825715a892838ca0ed83f4adc67c303
jeha00 <rudtls0611@naver.com>
2023-04-22 18:03:23 +0900

    FEAT: add model class and schema on Follow model

--- backend/sns/users/model.py
+++ backend/sns/users/model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Boolean
+from sqlalchemy import Column, String, Boolean, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 from sns.common.base import Base, BaseMixin
 
@@ -12,6 +13,29 @@ class User(Base, BaseMixin):
     profile_image_path = Column(String(200), nullable=True)
     verified = Column(Boolean, nullable=False, default=False)
     verification_code = Column(String(20), nullable=True, unique=True)
+    from_user = relationship(
+        "Follow", back_populates="follower", cascade="all, delete-orphan"
+    )
+    to_user = relationship(
+        "Follow", back_populates="following", cascade="all, delete-orphan"
+    )
 
     def __repr__(self) -> str:
         return f"User(id={self.id}, email={self.email}, name={self.name})"
+
+
+class Follow(Base, BaseMixin):
+    following_id = Column(Integer, ForeignKey("user.id"), ondelete="CASCADE")
+    following = relationship("User", back_populates="from_user")
+
+    follower_id = Column(Integer, ForeignKey("user.id"), ondelete="CASCADE")
+    follower = relationship("User", back_populates="to_user")
+
+    is_followed = Column(Boolean, nullable=False, default=True)
+
+    def __repr__(self) -> str:
+        return (
+            f"Follow(id={self.id}, "
+            f"following_id={self.following_id}, "
+            f"follower_id={self.follower_id})"
+        )
