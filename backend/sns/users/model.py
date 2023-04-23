fd5b76175b0d0c1963ec9573d78ac76e94d5865c
jeha00 <rudtls0611@naver.com>
2023-04-23 21:21:59 +0900

    FIX: fix ambiguous foreignkey error

--- backend/sns/users/model.py
+++ backend/sns/users/model.py
@@ -8,31 +8,45 @@ class User(Base, BaseMixin):
     email = Column(String(50), nullable=False, unique=True)
     password = Column(String(200), nullable=False)
     name = Column(String(20), nullable=False, unique=True)
+
+    verified = Column(Boolean, default=False)
+    verification_code = Column(String(20), nullable=True, unique=True)
+
     profile_text = Column(String(300), nullable=True)
     profile_image_name = Column(String(50), nullable=True)
     profile_image_path = Column(String(200), nullable=True)
-    verified = Column(Boolean, default=False)
-    verification_code = Column(String(20), nullable=True, unique=True)
+
     posts = relationship("Post", back_populates="writer", cascade="all, delete-orphan")
+
     from_user = relationship(
-        "Follow", back_populates="follower", cascade="all, delete-orphan"
+        "Follow",
+        back_populates="following",
+        foreign_keys="Follow.following_id",
+        cascade="all, delete-orphan",
     )
     to_user = relationship(
-        "Follow", back_populates="following", cascade="all, delete-orphan"
+        "Follow",
+        back_populates="follower",
+        foreign_keys="Follow.follower_id",
+        cascade="all, delete-orphan",
     )
 
     def __repr__(self) -> str:
-        return f"User(id={self.id}, email={self.email}, name={self.name})"
+        return f"User(id={self.id}, email={self.email})"
 
 
 class Follow(Base, BaseMixin):
+    is_followed = Column(Boolean, nullable=False, default=True)
+
     following_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
-    following = relationship("User", back_populates="from_user")
+    following = relationship(
+        "User", back_populates="from_user", foreign_keys=[following_id]
+    )
 
     follower_id = Column(Integer, ForeignKey("user.id", ondelete="CASCADE"))
-    follower = relationship("User", back_populates="to_user")
-
-    is_followed = Column(Boolean, nullable=False, default=True)
+    follower = relationship(
+        "User", back_populates="to_user", foreign_keys=[follower_id]
+    )
 
     def __repr__(self) -> str:
         return (
