abe3cbea5a0d6ca738f72fd05f6a5c751cf92163
jeha00 <rudtls0611@naver.com>
2023-05-10 22:04:42 +0900

    TEST: change test according to refactoring repositories/db.py and service.py

--- backend/sns/users/test/test_repository.py
+++ backend/sns/users/test/test_repository.py
@@ -1,44 +1,27 @@
 from typing import Dict
 
-import pytest
-from pydantic import ValidationError
-from datetime import datetime, timedelta
-from jose import jwt
-
-from fastapi.exceptions import HTTPException
 from fastapi.testclient import TestClient
+from pydantic import ValidationError
 from sqlalchemy.orm import Session
+import pytest
 
-from sns.common.config import settings
 from sns.users.test.utils import random_lower_string, random_email
 from sns.users.schema import UserCreate, UserUpdate
 from sns.users.repositories.db import user_crud
 from sns.users.service import user_service
 
 
-def test_create_access_token(client: TestClient, db_session: Session):
-    data = {"sub": random_email()}
-    to_encode = data.copy()
-
-    expire = datetime.utcnow() + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    data.update({"exp": expire})
-
-    encoded_jwt = jwt.encode(
-        data, settings.SECRET_KEY, algorithm=settings.SECRET_ALGORITHM
-    )
-    token = user_service.create_access_token(to_encode)
-
-    assert encoded_jwt == token
-
-
 def test_create_user(client: TestClient, db_session: Session):
     email = random_email()
     password = random_lower_string(k=8)
-    user_in = UserCreate(
-        email=email, password=password, password_confirm=password, verified=False
+    hashed_password = user_service.get_password_hash(password)
+    data_for_signup = UserCreate(
+        email=email,
+        password=hashed_password,
+        password_confirm=hashed_password,
+        verified=False,
     )
-    user = user_crud.create(db_session, user_info=user_in)
-
+    user = user_crud.create(db_session, data_for_signup=data_for_signup)
     assert user.email == email
     assert user.verified is False
     assert hasattr(user, "name")
@@ -61,49 +44,16 @@ def test_create_user(client: TestClient, db_session: Session):
         )
 
 
-def test_get_current_user(fake_user: Dict, db_session: Session):
-    user = fake_user.get("user")
-    data = {"sub": user.email}
-    token = user_service.create_access_token(data)
-    payload = jwt.decode(token, settings.SECRET_KEY, settings.SECRET_ALGORITHM)
-    email = payload.get("sub")
-
-    user_01 = user_crud.get_current_user(db_session, token)
-    user_02 = user_crud.get_user(db_session, email=email)
-
-    assert user_02
-    assert user == user_01
-    assert user == user_02
-    assert user_01 == user_02
-
-
-def test_get_current_user_verified(fake_user: Dict, db_session: Session):
-    user = fake_user.get("user")
-    info_to_be_updated = UserUpdate(verified=True, profile_text=None)
-    user_crud.update(db_session, user, info_to_be_updated)
-    verified_user = user_crud.get_current_user_verified(user)
-    assert verified_user == user
-
-
-def test_get_current_user_not_verified(fake_user: Dict, db_session: Session):
-    user = fake_user.get("user")
-    with pytest.raises(HTTPException):
-        user_crud.get_current_user_verified(user)
-
-
 def test_get_user(client: TestClient, db_session: Session):
     email = random_email()
     password = random_lower_string(k=8)
-    user_info = UserCreate(email=email, password=password, password_confirm=password)
-    user_created = user_crud.create(db_session, user_info=user_info)
-
-    user_by_only_email = user_crud.get_user(db_session, email=user_info.email)
-    assert user_by_only_email == user_created
-
-    user_by_email_and_password = user_crud.get_user(
-        db_session, email=user_info.email, password=user_info.password
+    data_for_signup = UserCreate(
+        email=email, password=password, password_confirm=password
     )
-    assert user_by_only_email == user_by_email_and_password
+    user_created = user_crud.create(db_session, data_for_signup=data_for_signup)
+
+    user_by_only_email = user_crud.get_user(db_session, email=data_for_signup.email)
+    assert user_by_only_email == user_created
 
 
 def test_update_user_on_being_verified(fake_user: Dict, db_session: Session):
@@ -158,7 +108,7 @@ def test_update_user_on_profile_text(fake_user: Dict, db_session: Session):
 def test_delete_user_by_int(fake_user: Dict, db_session: Session):
     user_01 = fake_user.get("user")
     email = user_01.email
-    result = user_crud.remove(db_session, user_info=user_01.id)
+    result = user_crud.remove(db_session, user_data=user_01.id)
     user_02 = user_crud.get_user(db_session, email=email)
 
     assert user_02 is None
@@ -169,7 +119,7 @@ def test_delete_user_by_int(fake_user: Dict, db_session: Session):
 def test_delete_user_by_model_object(fake_user: Dict, db_session: Session):
     user_01 = fake_user.get("user")
     email = user_01.email
-    result = user_crud.remove(db_session, user_info=user_01)
+    result = user_crud.remove(db_session, user_data=user_01)
     user_02 = user_crud.get_user(db_session, email=email)
 
     assert user_02 is None
