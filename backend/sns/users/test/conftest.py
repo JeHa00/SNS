82c3059f37196a078076d2b3611aa2a06b13071f
jeha00 <rudtls0611@naver.com>
2023-05-11 20:05:35 +0900

    TEST: refactor configure about test

--- backend/sns/users/test/conftest.py
+++ backend/sns/users/test/conftest.py
@@ -1,5 +1,6 @@
 from typing import Dict
 import pytest
+import copy
 
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
@@ -10,37 +11,40 @@ from sns.common.config import settings
 from sns.users.test.utils import random_lower_string, random_email
 from sns.users.schema import UserCreate, UserUpdate
 from sns.users.repositories.db import user_crud
+from sns.users.service import user_service
 
 
 @pytest.fixture(scope="function")
 def fake_user(client: TestClient, db_session: Session):
     password = random_lower_string(k=8)
-    user_info = UserCreate(
+    signup_data = UserCreate(
         email=random_email(), password=password, password_confirm=password
     )
-    user = user_crud.create(db_session, user_info=user_info)
-    return {"user": user, "user_info": user_info}
+    login_data = copy.deepcopy(signup_data)
+    user = user_service.create(db_session, data_for_signup=signup_data)
+    return {"user": user, "login_data": login_data}
 
 
 @pytest.fixture(scope="function")
-def get_user_token_headers_and_user_info(
+def get_user_token_headers_and_login_data(
     client: TestClient, db_session: Session
 ) -> Dict:
     # fake_user 생성
     email = random_email()
     password = random_lower_string(k=8)
-    user_info = UserCreate(email=email, password=password, password_confirm=password)
-    fake_user = user_crud.create(db_session, user_info=user_info)
+    signup_data = UserCreate(email=email, password=password, password_confirm=password)
+    login_data = copy.deepcopy(signup_data)
+    fake_user = user_service.create(db_session, data_for_signup=signup_data)
 
     # verified 업데이트
     info_to_be_updated = UserUpdate(verified=True)
     user_crud.update(db_session, fake_user, info_to_be_updated)
 
     # 로그인
-    login_info = {"email": email, "password": password}
-    response = client.post(f"{settings.API_V1_PREFIX}/login", json=login_info)
+    login_information = {"email": email, "password": password}
+    response = client.post(f"{settings.API_V1_PREFIX}/login", json=login_information)
 
     # headers 반환
     token = response.json().get("access_token")
     headers = {"Authorization": f"Bearer {token}"}
-    return {"headers": headers, "user_info": login_info}
+    return {"headers": headers, "login_data": login_data}
