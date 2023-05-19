d59eaa20d4d7c1afdc086bccd0238ec8230d0f4c
jeha00 <rudtls0611@naver.com>
2023-05-19 19:16:14 +0900

    REFACTOR: refactor controller and service on Post domain

--- backend/sns/posts/controller.py
+++ backend/sns/posts/controller.py
@@ -1,22 +1,30 @@
 from typing import List
 
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from sns.common.session import db
-from sns.users.service import user_service
+from sns.users.service import UserService
 from sns.users.schema import Msg
 from sns.users.model import User
 from sns.posts.schema import Post, PostCreate, PostUpdate
+from sns.posts.service import PostService
 from sns.posts import model
-from sns.posts.service import post_service
 
 
 router = APIRouter()
 
 
-@router.get("/posts/{post_id}", response_model=Post, status_code=status.HTTP_200_OK)
-def read_post(post_id: int, db: Session = Depends(db.get_db)) -> model.Post:
+@router.get(
+    "/posts/{post_id}",
+    response_model=Post,
+    status_code=status.HTTP_200_OK,
+)
+def read_post(
+    post_id: int,
+    post_service=Depends(PostService),
+    db: Session = Depends(db.get_db),
+) -> model.Post:
     """post_id와 일치하는 post.id를 가진 post 정보를 읽어온다.
 
      Args:
@@ -36,9 +44,16 @@ def read_post(post_id: int, db: Session = Depends(db.get_db)) -> model.Post:
 
 
 @router.get(
-    "/users/{user_id}/posts", response_model=List[Post], status_code=status.HTTP_200_OK
+    "/users/{user_id}/posts",
+    response_model=List[Post],
+    status_code=status.HTTP_200_OK,
 )
-def read_posts(user_id: int, db: Session = Depends(db.get_db)) -> List[Post]:
+def read_posts(
+    user_id: int,
+    user_service=Depends(UserService),
+    post_service=Depends(PostService),
+    db: Session = Depends(db.get_db),
+) -> List[Post]:
     """user_id에 일치하는 user가 작성한 글들을 조회
 
      Args:
@@ -55,18 +70,29 @@ def read_posts(user_id: int, db: Session = Depends(db.get_db)) -> List[Post]:
 
      - List[Post]: 여러 post가 list 배열에 담겨져 반환
     """
-    selected_user = user_service.get_user(db, user_id=user_id)
-    posts = post_service.get_multi_posts(db, writer_id=selected_user.id)
+    selected_user = user_service.get_user(
+        db,
+        user_id=user_id,
+    )
+    user_service.check_if_user_is_none(selected_user)
+    posts = post_service.get_multi_posts(
+        db,
+        writer_id=selected_user.id,
+    )
     return posts
 
 
 @router.post(
-    "/users/{user_id}/posts", response_model=Post, status_code=status.HTTP_201_CREATED
+    "/users/{user_id}/posts",
+    response_model=Post,
+    status_code=status.HTTP_201_CREATED,
 )
 def create_post(
     user_id: int,
     data_to_be_created: PostCreate,
-    current_user: User = Depends(user_service.get_current_user_verified),
+    user_service=Depends(UserService),
+    post_service=Depends(PostService),
+    current_user: User = Depends(UserService.get_current_user_verified),
     db: Session = Depends(db.get_db),
 ) -> Post:
     """user_id가 현재 로그인된 user와 동일할 때 post를 생성한다.
@@ -87,14 +113,18 @@ def create_post(
 
     - Post: 생성된 post 정보 반환
     """
-    user_service.get_user(db, user_id=user_id)
-    if user_id == current_user.id:
-        post = post_service.create(db, post_data=data_to_be_created, writer_id=user_id)
-        return post
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="작성할 권한이 없습니다."
-        )
+    user = user_service.get_user(
+        db,
+        user_id=user_id,
+    )
+    user_service.check_user_is_none(user)
+    post = post_service.create_post(
+        db,
+        user_id,
+        data_to_be_created,
+        current_user,
+    )
+    return post
 
 
 @router.put(
@@ -106,7 +136,9 @@ def update_post(
     user_id: int,
     post_id: int,
     data_to_be_updated: PostUpdate,
-    current_user: User = Depends(user_service.get_current_user_verified),
+    user_service=Depends(UserService),
+    post_service=Depends(PostService),
+    current_user: User = Depends(UserService.get_current_user_verified),
     db: Session = Depends(db.get_db),
 ) -> Post:
     """user_id가 현재 user id와 동일하여 수정 권한이 있을 때 post_id에 해당되는 post를 수정한다.
@@ -130,15 +162,19 @@ def update_post(
 
     - Post: 수정된 post 객체 반환
     """
-    user_service.get_user(db, user_id=user_id)
-    post = post_service.get_post(db, post_id=post_id)
-    if user_id == current_user.id and post.writer_id == current_user.id:
-        post_service.update(db, post, data_to_be_updated)
-        return post
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="수정할 권한이 없습니다."
-        )
+    user = user_service.get_user(
+        db,
+        user_id=user_id,
+    )
+    user_service.check_user_is_none(user)
+    post = post_service.update_post(
+        db,
+        user_id,
+        post_id,
+        data_to_be_updated,
+        current_user,
+    )
+    return post
 
 
 @router.delete(
@@ -149,7 +185,9 @@ def update_post(
 def delete_post(
     user_id: int,
     post_id: int,
-    current_user: User = Depends(user_service.get_current_user_verified),
+    user_service=Depends(UserService),
+    post_service=Depends(PostService),
+    current_user: User = Depends(UserService.get_current_user_verified),
     db: Session = Depends(db.get_db),
 ) -> Msg:
     """user_id가 current_user의 id와 동일할 때, 해당 post_id를 가진 post를 삭제한다.
@@ -172,12 +210,15 @@ def delete_post(
 
     - Msg: 삭제 성공 메세지를 반환
     """
-    user_service.get_user(db, user_id=user_id)
-    post = post_service.get_post(db, post_id=post_id)
-    if user_id == current_user.id and post.writer_id == current_user.id:
-        post_service.remove(db, post_to_be_deleted=post)
-        return {"status": "success", "msg": "글이 삭제되었습니다."}
-    else:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED, detail="삭제할 권한이 없습니다."
-        )
+    user = user_service.get_user(
+        db,
+        user_id=user_id,
+    )
+    user_service.check_user_is_none(user)
+    post_service.delete_post(
+        db,
+        user_id,
+        post_id,
+        current_user,
+    )
+    return {"status": "success", "msg": "글이 삭제되었습니다."}
--- backend/sns/posts/service.py
+++ backend/sns/posts/service.py
@@ -3,13 +3,18 @@ from typing import List
 from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 
+from sns.users.model import User
 from sns.posts.schema import PostCreate, PostUpdate
 from sns.posts.repository import post_crud
 from sns.posts.model import Post
 
 
 class PostService:
-    def get_post(self, db: Session, post_id: int) -> Post:
+    def get_post_and_check_none(
+        self,
+        db: Session,
+        post_id: int,
+    ) -> Post:
         """입력받은 정보를 PostDB class에 전달하여 post_id에 해당하는 Post 정보를 조회한다.
 
         Args:
@@ -21,17 +26,23 @@ class PostService:
         Returns:
             - Post: post_id에 해당되는 post 반환
         """
-        post = post_crud.get_post(db, post_id)
-
+        post = post_crud.get_post(
+            db,
+            post_id,
+        )
         if not post:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="해당 id의 글을 찾을 수 없습니다."
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="해당 id의 글을 찾을 수 없습니다.",
             )
-        else:
-            return post
+        return post
 
     def get_multi_posts(
-        self, db: Session, writer_id: int, skip: int = 0, limit: int = 10
+        self,
+        db: Session,
+        writer_id: int,
+        skip: int = 0,
+        limit: int = 10,
     ) -> List[Post]:
         """입력받은 정보를 PostDB class에 전달하여 writer_id 값에 해당되는 user가 작성한 여러 post들을 조회한다.
 
@@ -47,16 +58,27 @@ class PostService:
         Returns:
             - List[Post]: post 객체 정보들이 list 배열에 담겨져 반환
         """
-        posts = post_crud.get_multi_posts(db, writer_id, skip=skip, limit=limit)
+        posts = post_crud.get_multi_posts(
+            db,
+            writer_id,
+            skip=skip,
+            limit=limit,
+        )
 
         if len(posts) == 0:
             raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND, detail="작성된 글이 없습니다."
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="작성된 글이 없습니다.",
             )
         else:
             return posts
 
-    def create(self, db: Session, post_data: PostCreate, writer_id: int) -> Post:
+    def create(
+        self,
+        db: Session,
+        post_data: PostCreate,
+        writer_id: int,
+    ) -> Post:
         """입력받은 정보를 PostDB class에 전달하여 해당 정보를 가지는 post를 생성한다.
 
         Args:
@@ -70,7 +92,11 @@ class PostService:
             - Post: 생성된 post 정보를 반환
         """
         try:
-            created_post = post_crud.create(db, post_data, writer_id)
+            created_post = post_crud.create(
+                db,
+                post_data,
+                writer_id,
+            )
             return created_post
         except Exception:
             raise HTTPException(
@@ -79,7 +105,10 @@ class PostService:
             )
 
     def update(
-        self, db: Session, post_data: Post, data_to_be_updated: PostUpdate
+        self,
+        db: Session,
+        post_data: Post,
+        data_to_be_updated: PostUpdate,
     ) -> Post:
         """입력받은 정보를 PostDB class에 전달하여
            post_data에 해당되는 Post 객체에 data_to_be_updated 정보로 수정한다.
@@ -95,7 +124,11 @@ class PostService:
             - Post: 수정된 post 객체를 반환
         """
         try:
-            updated_post = post_crud.update(db, post_data, data_to_be_updated)
+            updated_post = post_crud.update(
+                db,
+                post_data,
+                data_to_be_updated,
+            )
             return updated_post
         except Exception:
             raise HTTPException(
@@ -103,7 +136,11 @@ class PostService:
                 detail="글 수정에 실패하였습니다.",
             )
 
-    def remove(self, db: Session, post_to_be_deleted: Post) -> bool:
+    def remove(
+        self,
+        db: Session,
+        post_to_be_deleted: Post,
+    ) -> bool:
         """입력받은 정보를 PostDB class에 전달하여 post_to_be_deleted 해당되는 post 객체를 삭제한다.
 
         Args:
@@ -116,12 +153,130 @@ class PostService:
             - bool: post 삭제에 성공하면 True를 반환
         """
         try:
-            post_crud.remove(db, post_to_be_deleted)
+            post_crud.remove(
+                db,
+                post_to_be_deleted,
+            )
             return True
         except Exception:
             raise HTTPException(
                 status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
                 detail="글 삭제에 실패하였습니다.",
+            )
+
+    def create_post(
+        self,
+        db: Session,
+        user_id: int,
+        data_to_be_created: PostCreate,
+        current_user: User,
+    ) -> Post:
+        """user_id가 current_user와 동일할 때 post를 생성한다.
+
+        Args:
+
+            - user_id (int): 글을 작성할 user의 id
+            - data_to_be_created (PostCreate): 생성할 post의 content 정보
+            - current_user (User, optional): 현재 로그인된 user 정보
+
+        Raises:
+
+            - HTTPException(500 INTERNAL SERVER ERROR): post 생성에 실패하면 발생
+            - HTTPException(401 UNAUTHORIZED): user_id가 로그인된 user id와 달라 작성 권한이 없으면 발생
+
+        Returns:
+
+              - Post: 생성된 post 정보 반환
+        """
+        if user_id == current_user.id:
+            post = self.create(db, data_to_be_created, user_id)
+            return post
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="작성할 권한이 없습니다.",
+            )
+
+    def update_post(
+        self,
+        db: Session,
+        user_id: int,
+        post_id: int,
+        data_to_be_updated: PostUpdate,
+        current_user: User,
+    ) -> Post:
+        """user_id가 현재 user id와 동일하여 수정 권한이 있을 때 post_id에 해당되는 post를 수정한다.
+
+        Args:
+
+            - user_id (int): 수정할 user의 id
+            - post_id (int): 수정될 post의 id
+            - data_to_be_updated (PostUpdate): 업데이트할 정보
+            - current_user (User): 현재 유저 정보
+
+        Raises:
+
+            - HTTPException(404 NOT FOUND): 다음 경우에 대해서 발생한다.
+                - post_id에 해당되는 post를 찾을 수 없을 때 발생
+            - HTTPException(500 INTERNAL SERVER ERROR): post 정보 변경에 실패했을 때 발생
+            - HTTPException(401 UNAUTHORIZED): user_id가 로그인된 user id와 달라 작성 권한이 없으면 발생
+
+        Returns:
+
+             - Post: 수정된 post 객체 반환
+        """
+        post = self.get_post_and_check_none(
+            db,
+            post_id,
+        )
+        if user_id == current_user.id and post.writer_id == current_user.id:
+            self.update(
+                db,
+                post,
+                data_to_be_updated,
+            )
+            return post
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="수정할 권한이 없습니다.",
+            )
+
+    def delete_post(
+        self,
+        db: Session,
+        user_id: int,
+        post_id: int,
+        current_user: User,
+    ) -> None:
+        """user_id가 current_user의 id와 동일할 때, 해당 post_id를 가진 post를 삭제한다.
+
+        Args:
+
+            - user_id (int): 삭제시킬 user의 id
+            - post_id (int): 삭제될 post의 id
+            - current_user (User): 현재 로그인된 user 정보
+
+        Raises:
+
+            - HTTPException(404 NOT FOUND): 다음 경우에 대해서 발생한다.
+                - post_id에 해당되는 post를 찾을 수 없을 때 발생
+            - HTTPException(500 INTERNAL SERVER ERROR): post 삭제에 실패했을 때 발생
+            - HTTPException(401 UNAUTHORIZED): user_id가 로그인된 user id와 달라 작성 권한이 없으면 발생
+        """
+        post = self.get_post_and_check_none(
+            db,
+            post_id=post_id,
+        )
+        if user_id == current_user.id and post.writer_id == current_user.id:
+            self.remove(
+                db,
+                post_to_be_deleted=post,
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="삭제할 권한이 없습니다.",
             )
 
 
