cd4ceeae89d946b96cf95794147ba49029e12864
jeha00 <rudtls0611@naver.com>
2023-07-21 00:14:39 +0900

    FEAT: update User model attribute - default value of nullable option is false

--- backend/sns/users/model.py
+++ backend/sns/users/model.py
@@ -4,13 +4,13 @@ from sns.common.base import Base, BaseMixin
 
 
 class User(Base, BaseMixin):
-    email = Column(String(50), nullable=False, unique=True)
+    email = Column(String(50), unique=True)
     password = Column(String(200), nullable=False)
-    name = Column(String(20), nullable=False, unique=True)
+    name = Column(String(20), unique=True)
     profile_text = Column(String(300), nullable=True)
     profile_image_name = Column(String(50), nullable=True)
     profile_image_path = Column(String(200), nullable=True)
-    verified = Column(Boolean, nullable=False, default=False)
+    verified = Column(Boolean, default=False)
     verification_code = Column(String(20), nullable=True, unique=True)
 
     def __repr__(self) -> str:
