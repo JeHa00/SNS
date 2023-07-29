4d4f0f8e9a83d85cae6c218da902de077e2de7fb
jeha00 <rudtls0611@naver.com>
2023-07-29 15:52:19 +0900

    FIX: fix start_app

--- backend/sns/common/conftest.py
+++ backend/sns/common/conftest.py
@@ -19,7 +19,7 @@ TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engin
 
 
 @pytest.fixture(scope="function")
-def start_app(start_app: FastAPI) -> Generator[FastAPI, Any, None]:
+def start_app() -> Generator[FastAPI, Any, None]:
     """
     test db와 연결된 app을 생성하여 기존에 만들었던 router를 연결한다.
     """
