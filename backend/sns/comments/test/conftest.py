b91655c7090f62b3c128030b48ef9e65757051b8
jeha00 <rudtls0611@naver.com>
2023-07-31 17:57:57 +0900

    CONF: remove fake_user_headers_and_info

--- backend/sns/comments/test/conftest.py
+++ backend/sns/comments/test/conftest.py
@@ -5,14 +5,16 @@ from sqlalchemy.orm import Session
 
 # flake8: noqa
 from sns.common.conftest import app, client, db_session, start_app
-from sns.common.config import settings
 
 # flake8: noqa
-from sns.users.test.conftest import fake_user, get_user_token_headers_and_user_info
+from sns.users.test.conftest import fake_user, get_user_token_headers_and_login_data
+
 from sns.users.test.utils import random_lower_string
-from sns.users.schema import UserUpdate
-from sns.users.service import update, get_user
+
+# flake8: noqa
 from sns.posts.test.conftest import fake_post
+
+from sns.users.service import user_service
 from sns.posts.model import Post
 from sns.comments.repository import comment_crud
 from sns.comments.schema import CommentCreate
@@ -21,22 +23,30 @@ from sns.comments.model import Comment
 
 @pytest.fixture(scope="function")
 def fake_comment(
-    client: TestClient, db_session: Session, fake_user: dict, fake_post: Post
+    client: TestClient,
+    db_session: Session,
+    fake_user: dict,
+    fake_post: Post,
 ) -> Comment:
     # writer 정보
     writer = fake_user.get("user")
 
+    # verified 업데이트
+    user_service.update(db_session, writer, {"verified": True})
+
     # content 정보
     content = CommentCreate(
-        content=random_lower_string(k=500), writer_id=writer.id, post_id=fake_post.id
+        content=random_lower_string(k=500),
+        writer_id=writer.id,
+        post_id=fake_post.id,
     )
 
     # comment 생성
     comment = comment_crud.create(
         db_session,
-        data_to_be_created=content,
-        writer_id=writer.id,
-        post_id=fake_post.id,
+        writer.id,
+        fake_post.id,
+        **content.dict(),
     )
 
     return comment
@@ -44,7 +54,10 @@ def fake_comment(
 
 @pytest.fixture(scope="function")
 def fake_multi_comments(
-    client: TestClient, db_session: Session, fake_user: dict, fake_post: Post
+    client: TestClient,
+    db_session: Session,
+    fake_user: dict,
+    fake_post: Post,
 ) -> None:
     # writer 정보
     writer = fake_user.get("user")
@@ -61,31 +74,8 @@ def fake_multi_comments(
         )
         comment_crud.create(
             db_session,
-            data_to_be_created=content,
-            writer_id=writer.id,
-            post_id=fake_post.id,
+            writer.id,
+            fake_post.id,
+            **content.dict(),
         )
         total_comment_count_to_produce -= 1
-
-
-@pytest.fixture(scope="function")
-def fake_user_headers_and_info(
-    client: TestClient, db_session: Session, fake_user: dict
-):
-    # 유저 정보
-    user_info = fake_user.get("user_info")  # 유저 이메일 및 해쉬 전 비밀번호 정보
-    user = get_user(db_session, email=user_info.email)
-    # BUG: get_user가 아닌 fake_user.get("user") 로 User 정보를 가져올 경우 jsonable_encoder가 빈 객체를 반환.
-
-    # verified 업데이트
-    info_to_be_updated = UserUpdate(verified=True)
-    update(db_session, user, info_to_be_updated)
-
-    # 로그인
-    login_info = {"email": user_info.email, "password": user_info.password}
-    response = client.post(f"{settings.API_V1_STR}/login", json=login_info)
-
-    # headers 반환
-    token = response.json().get("access_token")
-    headers = {"Authorization": f"Bearer {token}"}
-    return {"headers": headers, "user_info": login_info}
