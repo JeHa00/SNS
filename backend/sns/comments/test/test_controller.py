8f39c64d05dc59c58245001e227d9dd89263a844
jeha00 <rudtls0611@naver.com>
2023-07-31 17:58:33 +0900

    REFACTOR: refactor test_controller, test_service

--- backend/sns/comments/test/test_controller.py
+++ backend/sns/comments/test/test_controller.py
@@ -1,13 +1,12 @@
 import pytest
 
-from fastapi.encoders import jsonable_encoder
 from fastapi.testclient import TestClient
 from fastapi import status
 from sqlalchemy.orm import Session
 
 from sns.common.config import settings
 from sns.users.test.utils import random_lower_string, random_email
-from sns.users.service import get_user, create
+from sns.users.service import user_service
 from sns.users.schema import UserCreate
 from sns.posts.model import Post
 from sns.comments.model import Comment
@@ -15,56 +14,106 @@ from sns.comments.schema import CommentCreate, CommentUpdate
 
 
 @pytest.mark.get_comments_on_a_post
-def test_comments_on_a_post_if_post_not_exist(client: TestClient, db_session: Session):
+def test_comments_on_a_post_if_post_not_exist(
+    client: TestClient,
+    db_session: Session,
+):
     post_id = 1
-    response = client.get(f"{settings.API_V1_PREFIX}/posts/{post_id}/comments")
+    page = 1
+    response = client.get(
+        f"{settings.API_V1_PREFIX}/posts/{post_id}/comments?page={page}",
+    )
     result_msg = response.json().get("detail")
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert result_msg == "해당 id의 포스트를 찾을 수 없습니다."
-
-
-@pytest.mark.get_comments_on_a_post
-def test_comments_on_a_post_if_post_exist(
-    client: TestClient, db_session: Session, fake_post: Post, fake_multi_comments: None
-):
-    response = client.get(f"{settings.API_V1_PREFIX}/posts/{fake_post.id}/comments")
-
-    assert response.status_code == status.HTTP_200_OK
-    assert len(response.json()) == 100
+    assert result_msg == "해당 id의 글을 찾을 수 없습니다."
 
 
 @pytest.mark.get_comments_of_a_user
-def test_comments_of_a_user_if_not_registered(client: TestClient, db_session: Session):
-    user_id = 1
-    response = client.get(f"{settings.API_V1_PREFIX}/users/{user_id}/comments")
+def test_comments_of_a_post_if_post_exist_but_comments_not_exist(
+    client: TestClient,
+    db_session: Session,
+    fake_post: Post,
+):
+    page = 1
+    response = client.get(
+        f"{settings.API_V1_PREFIX}/posts/{fake_post.id}/comments?page={page}",
+    )
     result_msg = response.json().get("detail")
 
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert result_msg == "등록된 회원이 아닙니다."
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert result_msg == "해당 글에 작성된 댓글이 없습니다."
+
+
+@pytest.mark.get_comments_on_a_post
+def test_comments_on_a_post_if_post_and_comments_exist(
+    client: TestClient,
+    db_session: Session,
+    fake_post: Post,
+    fake_multi_comments: None,
+):
+    for page in range(4):
+        response = client.get(
+            f"{settings.API_V1_PREFIX}/posts/{fake_post.id}/comments?page={page}",
+        )
+        assert response.status_code == status.HTTP_200_OK
+        if page != 3:
+            assert len(response.json()) == 30
+        else:
+            assert len(response.json()) == 10
+
+
+@pytest.mark.get_comments_of_a_user
+def test_comments_of_a_user_if_not_registered(
+    client: TestClient,
+    db_session: Session,
+):
+    user_id = 1
+    page = 1
+    response = client.get(
+        f"{settings.API_V1_PREFIX}/users/{user_id}/comments?page={page}",
+    )
+    result_msg = response.json().get("detail")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert result_msg == "해당되는 유저를 찾을 수 없습니다."
 
 
 @pytest.mark.get_comments_of_a_user
 def test_comments_of_a_user_if_comment_not_exist(
-    client: TestClient, db_session: Session, fake_user: dict
+    client: TestClient,
+    db_session: Session,
+    fake_user: dict,
 ):
+    page = 1
     user = fake_user.get("user")
-    response = client.get(f"{settings.API_V1_PREFIX}/users/{user.id}/comments")
+    response = client.get(
+        f"{settings.API_V1_PREFIX}/users/{user.id}/comments?page={page}",
+    )
     result_msg = response.json().get("detail")
 
     assert response.status_code == status.HTTP_404_NOT_FOUND
-    assert result_msg == "작성된 댓글이 없습니다."
+    assert result_msg == "해당 유저가 작성한 댓글이 없습니다."
 
 
 @pytest.mark.get_comments_of_a_user
 def test_comments_of_a_user_if_comments_exist(
-    client: TestClient, db_session: Session, fake_user: dict, fake_multi_comments: None
+    client: TestClient,
+    db_session: Session,
+    fake_user: dict,
+    fake_multi_comments: None,
 ):
-    user = fake_user.get("user")
-    response = client.get(f"{settings.API_V1_PREFIX}/users/{user.id}/comments")
+    for page in range(1, 4):
+        user = fake_user.get("user")
+        response = client.get(
+            f"{settings.API_V1_PREFIX}/users/{user.id}/comments?page={page}",
+        )
 
-    assert response.status_code == status.HTTP_200_OK
-    assert len(response.json()) == 100
+        assert response.status_code == status.HTTP_200_OK
+        if page != 3:
+            assert len(response.json()) == 30
+        else:
+            assert len(response.json()) == 10
 
 
 @pytest.mark.create_comment
@@ -72,28 +121,27 @@ def test_create_comment_if_not_registered(
     client: TestClient,
     db_session: Session,
     fake_post: Post,
-    fake_user_headers_and_info: dict,
+    get_user_token_headers_and_login_data: dict,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
+    headers = get_user_token_headers_and_login_data.get("headers")
 
     # not registered user
-    user_id = 2
+    user_id = 3
 
     # content 내용
-    content = CommentCreate(content=random_lower_string(k=500))
-    data = jsonable_encoder(content)
+    data_to_be_created = CommentCreate(content=random_lower_string(k=500))
 
     # comment 생성 및 결과
     response = client.post(
         f"{settings.API_V1_PREFIX}/users/{user_id}/posts/{fake_post.id}/comments",
         headers=headers,
-        json=data,
+        json=data_to_be_created.dict(),
     )
     result_msg = response.json().get("detail")
 
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert result_msg == "등록된 회원이 아닙니다."
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert result_msg == "해당되는 유저를 찾을 수 없습니다."
 
 
 @pytest.mark.create_comment
@@ -101,27 +149,30 @@ def test_create_comment_if_unauthorized(
     client: TestClient,
     db_session: Session,
     fake_post: Post,
-    fake_user_headers_and_info: dict,
+    get_user_token_headers_and_login_data: dict,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
+    headers = get_user_token_headers_and_login_data.get("headers")
 
     # unauthorized user 생성
     password = random_lower_string(k=8)
-    user_info = UserCreate(
-        email=random_email(), password=password, password_confirm=password
+    data_for_signup = UserCreate(
+        email=random_email(),
+        password=password,
+        password_confirm=password,
     )
-    user = create(db_session, user_info=user_info)
-
+    user = user_service.create(
+        db_session,
+        data_for_signup.dict(),
+    )
     # content 내용
-    content = CommentCreate(content=random_lower_string(k=500))
-    data = jsonable_encoder(content)
+    data_to_be_created = CommentCreate(content=random_lower_string(k=500))
 
     # content 생성 및 결과
     response = client.post(
         f"{settings.API_V1_PREFIX}/users/{user.id}/posts/{fake_post.id}/comments",
         headers=headers,
-        json=data,
+        json=data_to_be_created.dict(),
     )
     result_msg = response.json().get("detail")
 
@@ -133,91 +184,87 @@ def test_create_comment_if_unauthorized(
 def test_create_comment_if_authorized(
     client: TestClient,
     db_session: Session,
-    fake_user_headers_and_info: dict,
+    get_user_token_headers_and_login_data: dict,
     fake_post: Post,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
-    user_info = fake_user_headers_and_info.get("user_info")
-    email = user_info.get("email")
-    user = get_user(db_session, email=email)
+    headers = get_user_token_headers_and_login_data.get("headers")
+    login_data = get_user_token_headers_and_login_data.get("login_data")
+    email = login_data.get("email")
+    user = user_service.get_user(db_session, email=email)
 
     # content 내용
-    content = CommentCreate(content=random_lower_string(k=500))
-    data = jsonable_encoder(content)
+    data_for_signup = CommentCreate(content=random_lower_string(k=500))
 
     # comment 생성 및 결과
     response = client.post(
         f"{settings.API_V1_PREFIX}/users/{user.id}/posts/{fake_post.id}/comments",
         headers=headers,
-        json=data,
+        json=data_for_signup.dict(),
     )
-    content_of_comment_created = response.json().get("content")
+    content_of_comment_created = response.json()["content"]
 
     assert response.status_code == status.HTTP_201_CREATED
-    assert content_of_comment_created == data.get("content")
+    assert content_of_comment_created == data_for_signup.content
 
 
 @pytest.mark.update_comment
 def test_update_comment_if_not_registered(
     client: TestClient,
     db_session: Session,
-    fake_user_headers_and_info: dict,
+    get_user_token_headers_and_login_data: dict,
     fake_comment: Comment,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
+    headers = get_user_token_headers_and_login_data.get("headers")
 
     # not registered user
-    user_id = 2
+    user_id = 3
 
     # content 내용
-    content = CommentUpdate(
+    data_to_be_updated = CommentUpdate(
         content=random_lower_string(k=500),
     )
-    data = jsonable_encoder(content)
 
     # content 내용 수정 및 결과
     response = client.put(
         f"{settings.API_V1_PREFIX}/users/{user_id}/comments/{fake_comment.id}",
         headers=headers,
-        json=data,
+        json=data_to_be_updated.dict(),
     )
-    result_msg = response.json().get("detail")
+    result_msg = response.json()["detail"]
 
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert result_msg == "등록된 회원이 아닙니다."
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert result_msg == "해당되는 유저를 찾을 수 없습니다."
 
 
 @pytest.mark.update_comment
 def test_update_comment_if_unauthorized(
     client: TestClient,
     db_session: Session,
+    get_user_token_headers_and_login_data: dict,
     fake_comment: Comment,
-    fake_user_headers_and_info: dict,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
-    user_info = fake_user_headers_and_info.get("user_info")
+    headers = get_user_token_headers_and_login_data.get("headers")
 
     # unauthorized user 생성
     password = random_lower_string(k=8)
-    user_info = UserCreate(
-        email=random_email(), password=password, password_confirm=password
+    data_for_signup = UserCreate(
+        email=random_email(),
+        password=password,
+        password_confirm=password,
     )
-    user = create(db_session, user_info=user_info)
+    user = user_service.create(db_session, data_for_signup.dict())
 
     # content 내용
-    content = CommentUpdate(
-        content=random_lower_string(k=500),
-    )
-    data = jsonable_encoder(content)
+    data_to_be_updated = CommentUpdate(content=random_lower_string(k=500))
 
     # content 내용 수정 및 결과
     response = client.put(
         f"{settings.API_V1_PREFIX}/users/{user.id}/comments/{fake_comment.id}",
         headers=headers,
-        json=data,
+        json=data_to_be_updated.dict(),
     )
     result_msg = response.json().get("detail")
 
@@ -229,30 +276,36 @@ def test_update_comment_if_unauthorized(
 def test_update_comment_if_authorized(
     client: TestClient,
     db_session: Session,
+    fake_user: dict,
     fake_comment: Comment,
-    fake_user_headers_and_info: dict,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
-    user_info = fake_user_headers_and_info.get("user_info")
-    email = user_info.get("email")
-    user = get_user(db_session, email=email)
+    writer = fake_user.get("user")
+    login_data = fake_user.get("login_data")
+
+    # 로그인 및 JWT 생성
+    login_data = {
+        "email": login_data["email"],
+        "password": login_data["password"],
+    }
+    response = client.post(f"{settings.API_V1_PREFIX}/login", json=login_data)
+    token = response.json()["access_token"]
+    headers = {"Authorization": f"Bearer {token}"}
 
     # content 내용
-    content = CommentUpdate(
+    data_to_be_updated = CommentUpdate(
         content=random_lower_string(k=500),
     )
-    data = jsonable_encoder(content)
 
     # content 내용 수정 및 결과
     response = client.put(
-        f"{settings.API_V1_PREFIX}/users/{user.id}/comments/{fake_comment.id}",
+        f"{settings.API_V1_PREFIX}/users/{writer.id}/comments/{fake_comment.id}",
         headers=headers,
-        json=data,
+        json=data_to_be_updated.dict(),
     )
 
     assert response.status_code == status.HTTP_200_OK
-    assert response.json().get("content") == data.get("content")
+    assert response.json().get("content") == data_to_be_updated.content
 
 
 @pytest.mark.delete_comment
@@ -260,13 +313,13 @@ def test_delete_comment_if_not_registered(
     client: TestClient,
     db_session: Session,
     fake_comment: Comment,
-    fake_user_headers_and_info: dict,
+    get_user_token_headers_and_login_data: dict,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
+    headers = get_user_token_headers_and_login_data.get("headers")
 
     # not registered user
-    user_id = 2
+    user_id = 3
 
     # 댓글 삭제 및 결과
     response = client.delete(
@@ -275,8 +328,8 @@ def test_delete_comment_if_not_registered(
     )
     result_msg = response.json().get("detail")
 
-    assert response.status_code == status.HTTP_403_FORBIDDEN
-    assert result_msg == "등록된 회원이 아닙니다."
+    assert response.status_code == status.HTTP_404_NOT_FOUND
+    assert result_msg == "해당되는 유저를 찾을 수 없습니다."
 
 
 @pytest.mark.delete_comment
@@ -284,21 +337,17 @@ def test_delete_comment_if_not_unauthorized(
     client: TestClient,
     db_session: Session,
     fake_comment: Comment,
-    fake_user_headers_and_info: dict,
+    get_user_token_headers_and_login_data: dict,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
+    headers = get_user_token_headers_and_login_data.get("headers")
 
     # unauthorized user 생성
-    password = random_lower_string(k=8)
-    user_info = UserCreate(
-        email=random_email(), password=password, password_confirm=password
-    )
-    user = create(db_session, user_info=user_info)
+    writer_id = 1  # fake_comment 생성 시 호출되는 fake_user의 id
 
     # 댓글 삭제 및 결과
     response = client.delete(
-        f"{settings.API_V1_PREFIX}/users/{user.id}/comments/{fake_comment.id}",
+        f"{settings.API_V1_PREFIX}/users/{writer_id}/comments/{fake_comment.id}",
         headers=headers,
     )
     result_msg = response.json().get("detail")
@@ -311,18 +360,25 @@ def test_delete_comment_if_not_unauthorized(
 def test_delete_comment_if_authorized(
     client: TestClient,
     db_session: Session,
+    fake_user: dict,
     fake_comment: Comment,
-    fake_user_headers_and_info: dict,
 ):
     # current_user 정보
-    headers = fake_user_headers_and_info.get("headers")
-    user_info = fake_user_headers_and_info.get("user_info")
-    email = user_info.get("email")
-    user = get_user(db_session, email=email)
+    writer = fake_user.get("user")
+    login_data = fake_user.get("login_data")
+
+    # 로그인 및 JWT 생성
+    login_data = {
+        "email": login_data["email"],
+        "password": login_data["password"],
+    }
+    response = client.post(f"{settings.API_V1_PREFIX}/login", json=login_data)
+    token = response.json()["access_token"]
+    headers = {"Authorization": f"Bearer {token}"}
 
     # 댓글 삭제 및 결과
     response = client.delete(
-        f"{settings.API_V1_PREFIX}/users/{user.id}/comments/{fake_comment.id}",
+        f"{settings.API_V1_PREFIX}/users/{writer.id}/comments/{fake_comment.id}",
         headers=headers,
     )
     result_status = response.json().get("status")
--- backend/sns/comments/test/test_repository.py
+++ backend/sns/comments/test/test_repository.py
@@ -1,32 +1,37 @@
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
-from sns.users.service import delete as user_remove
 from sns.users.test.utils import random_lower_string
+from sns.users.service import user_service
+from sns.posts.service import post_service
 from sns.posts.model import Post
-from sns.posts.service import remove as post_remove
-from sns.comments.model import Comment
 from sns.comments.repository import comment_crud
-from sns.comments.schema import CommentCreate, CommentUpdate
+from sns.comments.model import Comment
+from sns.comments import schema
 
 
 def test_create(
-    client: TestClient, db_session: Session, fake_user: dict, fake_post: Post
+    client: TestClient,
+    db_session: Session,
+    fake_user: dict,
+    fake_post: Post,
 ):
     # 유저 정보
     user = fake_user.get("user")
 
     # 댓글 내용
-    data_to_be_created = CommentCreate(
-        content=random_lower_string(k=500), writer_id=user.id, post_id=fake_post.id
+    data_to_be_created = schema.CommentCreate(
+        content=random_lower_string(k=500),
+        writer_id=user.id,
+        post_id=fake_post.id,
     )
 
     # 생성한 댓글
     comment = comment_crud.create(
         db_session,
-        data_to_be_created=data_to_be_created,
-        writer_id=user.id,
-        post_id=fake_post.id,
+        user.id,
+        fake_post.id,
+        **data_to_be_created.dict(),
     )
 
     assert comment is not None
@@ -48,10 +53,9 @@ def test_create(
 
 def test_get_comment(client: TestClient, db_session: Session, fake_comment: Comment):
     # 댓글 정보
-    comment = comment_crud.get_comment(db_session, comment_id=fake_comment.id)
+    comment = comment_crud.get_comment(db_session, fake_comment.id)
 
     assert comment is not None
-    assert comment.id == 1
 
 
 def test_get_multi_comments_by_writer_id(
@@ -65,10 +69,14 @@ def test_get_multi_comments_by_writer_id(
     user = fake_user.get("user")
 
     # 생성한 댓글들
-    comments = comment_crud.get_multi_comments(db_session, writer_id=user.id)
+    comments = comment_crud.get_multi_comments(
+        db_session,
+        None,
+        user.id,
+    )
 
     assert comments is not None
-    assert len(comments) == 100
+    assert len(comments) == 30
 
     for comment in comments:
         assert hasattr(comment, "id")
@@ -97,10 +105,10 @@ def test_get_multi_comments_by_post_id(
     user = fake_user.get("user")
 
     # 생성한 댓글들
-    comments = comment_crud.get_multi_comments(db_session, post_id=fake_post.id)
+    comments = comment_crud.get_multi_comments(db_session, fake_post.id, None)
 
     assert comments is not None
-    assert len(comments) == 100
+    assert len(comments) == 30
 
     for comment in comments:
         assert hasattr(comment, "id")
@@ -118,50 +126,24 @@ def test_get_multi_comments_by_post_id(
         assert hasattr(comment, "updated_at")
 
 
-def test_update_only_one_by_int(
-    client: TestClient, db_session: Session, fake_comment: Comment
-):
-    new_content = random_lower_string(k=500)
-    data_to_be_updated = CommentUpdate(content=new_content)
-    comment = comment_crud.update(
-        db_session, comment_info=fake_comment.id, data_to_be_updated=data_to_be_updated
-    )
-    assert comment is not None
-    assert comment.id == fake_comment.id
-    assert comment.content == new_content
-
-
-def test_update_only_one_by_model_object(
-    client: TestClient, db_session: Session, fake_comment: Comment
-):
-    new_content = random_lower_string(k=500)
-    data_to_be_updated = CommentUpdate(content=new_content)
-    comment = comment_crud.update(
-        db_session, comment_info=fake_comment, data_to_be_updated=data_to_be_updated
-    )
-    assert comment is not None
-    assert comment.id == fake_comment.id
-    assert comment.content == new_content
-
-
-def test_update_multi_comments_by_int(
+def test_update_only_one(
     client: TestClient,
     db_session: Session,
-    fake_multi_comments: None,
+    fake_comment: Comment,
 ):
     new_content = random_lower_string(k=500)
-    content = CommentUpdate(content=new_content)
+    data_to_be_updated = schema.CommentUpdate(content=new_content)
+    comment = comment_crud.update(
+        db_session,
+        fake_comment,
+        **data_to_be_updated.dict(),
+    )
+    assert comment is not None
+    assert comment.id == fake_comment.id
+    assert comment.content == new_content
 
-    for comment_id in range(1, 101):
-        comment = comment_crud.update(
-            db_session, comment_info=comment_id, data_to_be_updated=content
-        )
-        assert comment is not None
-        assert comment.content == new_content
 
-
-# NOTE: Post domain에서 발생한 것과 동일한 문제
-def test_update_multi_comments_by_model_object(
+def test_update_multi_comments(
     client: TestClient,
     db_session: Session,
     fake_post: Post,
@@ -169,35 +151,52 @@ def test_update_multi_comments_by_model_object(
 ):
     old_content = fake_post.content
     new_content = random_lower_string(k=500)
-    content = CommentUpdate(content=new_content)
-    comments = comment_crud.get_multi_comments(db_session, post_id=fake_post.id)
+    data_to_be_updated = schema.CommentUpdate(content=new_content)
+    comments = comment_crud.get_multi_comments(db_session, fake_post.id, None)
 
-    assert len(comments) == 100
+    assert len(comments) == 30
 
     for comment_model_object in comments:
-        assert hasattr(comment_model_object, "content")  # NOTE: 이게 없으면 FAILED 발생
         comment = comment_crud.update(
-            db_session, comment_info=comment_model_object, data_to_be_updated=content
+            db_session,
+            comment_model_object,
+            **data_to_be_updated.dict(),
         )
         assert comment.content != old_content
         assert comment.content == new_content
 
 
 def test_remove_only_one_by_int(
-    client: TestClient, db_session: Session, fake_comment: Comment
+    client: TestClient,
+    db_session: Session,
+    fake_comment: Comment,
 ):
     comment_id = fake_comment.id
-    comment_crud.remove(db_session, comment_info=comment_id)
-    comment = comment_crud.get_comment(db_session, comment_id=comment_id)
+    comment_crud.remove(
+        db_session,
+        comment_id,
+    )
+    comment = comment_crud.get_comment(
+        db_session,
+        comment_id,
+    )
 
     assert comment is None
 
 
 def test_remove_only_one_by_model_object(
-    client: TestClient, db_session: Session, fake_comment: Comment
+    client: TestClient,
+    db_session: Session,
+    fake_comment: Comment,
 ):
-    comment_crud.remove(db_session, comment_info=fake_comment)
-    comment = comment_crud.get_comment(db_session, comment_id=fake_comment.id)
+    comment_crud.remove(
+        db_session,
+        fake_comment,
+    )
+    comment = comment_crud.get_comment(
+        db_session,
+        fake_comment.id,
+    )
 
     assert comment is None
 
@@ -208,13 +207,23 @@ def test_remove_multi_comments_by_int(
     fake_post: Post,
     fake_multi_comments: None,
 ):
-    comments = comment_crud.get_multi_comments(db_session, post_id=fake_post.id)
+    comments = comment_crud.get_multi_comments(
+        db_session,
+        fake_post.id,
+        None,
+    )
 
-    assert len(comments) == 100
+    assert len(comments) == 30
 
     for comment in comments:
-        comment_crud.remove(db_session, comment_info=comment.id)
-        comment = comment_crud.get_comment(db_session, comment_id=comment.id)
+        comment_crud.remove(
+            db_session,
+            comment.id,
+        )
+        comment = comment_crud.get_comment(
+            db_session,
+            comment.id,
+        )
 
         assert comment is None
 
@@ -225,13 +234,23 @@ def test_remove_multi_comments_by_model_object(
     fake_post: Post,
     fake_multi_comments: None,
 ):
-    comments = comment_crud.get_multi_comments(db_session, post_id=fake_post.id)
+    comments = comment_crud.get_multi_comments(
+        db_session,
+        fake_post.id,
+        None,
+    )
 
-    assert len(comments) == 100
+    assert len(comments) == 30
 
     for comment in comments:
-        comment_crud.remove(db_session, comment_info=comment)
-        comment = comment_crud.get_comment(db_session, comment_id=comment.id)
+        comment_crud.remove(
+            db_session,
+            comment,
+        )
+        comment = comment_crud.get_comment(
+            db_session,
+            comment.id,
+        )
 
         assert comment is None
 
@@ -243,12 +262,20 @@ def test_remove_multi_comments_by_deleting_user(
     fake_multi_comments: None,
 ):
     user = fake_user.get("user")
-    comments = comment_crud.get_multi_comments(db_session, writer_id=user.id)
+    comments = comment_crud.get_multi_comments(
+        db_session,
+        None,
+        user.id,
+    )
 
-    assert len(comments) == 100
+    assert len(comments) == 30
 
-    user_remove(db_session, user_info=user)
-    comments = comment_crud.get_multi_comments(db_session, writer_id=user.id)
+    user_service.remove(db_session, user)
+    comments = comment_crud.get_multi_comments(
+        db_session,
+        None,
+        user.id,
+    )
 
     assert len(comments) == 0
 
@@ -259,11 +286,22 @@ def test_remove_multi_comments_by_deleting_post(
     fake_post: Post,
     fake_multi_comments: None,
 ):
-    comments = comment_crud.get_multi_comments(db_session, post_id=fake_post.id)
+    comments = comment_crud.get_multi_comments(
+        db_session,
+        fake_post.id,
+        None,
+    )
 
-    assert len(comments) == 100
+    assert len(comments) == 30
 
-    post_remove(db_session, post_info=fake_post)
-    comments = comment_crud.get_multi_comments(db_session, post_id=fake_post.id)
+    post_service.remove(
+        db_session,
+        fake_post,
+    )
+    comments = comment_crud.get_multi_comments(
+        db_session,
+        fake_post.id,
+        None,
+    )
 
     assert len(comments) == 0
