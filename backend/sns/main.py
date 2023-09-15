755f700256246e610a385bcbf77163455ab205ff
jeha00 <rudtls0611@naver.com>
2023-09-15 19:43:19 +0900

    FEAT: add router on Comment domain

--- backend/sns/main.py
+++ backend/sns/main.py
@@ -7,9 +7,9 @@ from sns.common.config import settings
 from sns.common.session import db
 from sns.users.controller import router as users
 from sns.posts.controller import router as posts
+from sns.comments.controller import router as comments
 
-# from sns.comments.controller import router as comments
-# from sns.notification.controller import router as notification
+# from sns.notification.controller import router as notifications
 
 app = FastAPI(title=settings.PROJECT_NAME, openapi_url=f"{settings.API_V1_PREFIX}")
 db.init_app(app)
@@ -26,8 +26,8 @@ if settings.BACKEND_CORS_ORIGINS:
 
 app.include_router(users, tags=["Users"], prefix=settings.API_V1_PREFIX)
 app.include_router(posts, tags=["Posts"], prefix=settings.API_V1_PREFIX)
-# app.include_router(comments, tags=['Comments'], prefix=settings.API_V1_PREFIX)
-# app.include_router(notification, tags=['Notification'], prefix=settings.API_V1_PREFIX)
+app.include_router(comments, tags=["Comments"], prefix=settings.API_V1_PREFIX)
+# app.include_router(notification, tags=["Notification"], prefix=settings.API_V1_PREFIX)
 
 
 @app.exception_handler(RequestValidationError)
