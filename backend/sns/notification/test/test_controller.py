367891576cd2280799fd6981356c3396f5ebddcb
jeha00 <rudtls0611@naver.com>
2023-10-17 22:03:38 +0900

    TEST: update tests mark_as_Read

--- backend/sns/notification/test/test_controller.py
+++ backend/sns/notification/test/test_controller.py
@@ -1,19 +1,33 @@
 from fastapi.testclient import TestClient
+from sqlalchemy.orm import Session
 from fastapi import status
 import pytest
 
 from sns.common.config import settings
+from sns.users.service import user_service
 
 
 @pytest.mark.mark_as_read
 def test_mark_as_read_if_follower_is_not_same_as_current_user(
     client: TestClient,
+    db_session: Session,
     fake_follow_notifications: None,
-    get_user_token_headers_and_login_data: dict,
+    fake_user: dict,
 ):
-    total_fake_follow_data_count = 64
-    headers = get_user_token_headers_and_login_data.get("headers")
+    total_fake_follow_data_count = 9
 
+    # token 발행
+    user_logged_in = fake_user.get("user")
+    login_data = fake_user.get("login_data")
+
+    user_service.update(db_session, user_logged_in, {"verified": True})
+
+    response = client.post(f"{settings.API_V1_PREFIX}/login", json=login_data)
+
+    token = response.json()["access_token"]
+    headers = {"Authorization": f"Bearer {token}"}
+
+    # 읽음 표시로 변경
     for notification_id in range(1, total_fake_follow_data_count + 1):
         response = client.patch(
             f"{settings.API_V1_PREFIX}/notifications/{notification_id}",
@@ -29,12 +43,24 @@ def test_mark_as_read_if_follower_is_not_same_as_current_user(
 @pytest.mark.mark_as_read
 def test_mark_as_read_if_writer_is_not_same_as_current_user(
     client: TestClient,
-    get_user_token_headers_and_login_data: dict,
+    db_session: Session,
     fake_postlike_notifications: None,
+    fake_user: dict,
 ):
-    total_fake_post_like_data_count = 150
-    headers = get_user_token_headers_and_login_data.get("headers")
+    total_fake_post_like_data_count = 100
 
+    # token 발행
+    user_logged_in = fake_user.get("user")
+    login_data = fake_user.get("login_data")
+
+    user_service.update(db_session, user_logged_in, {"verified": True})
+
+    response = client.post(f"{settings.API_V1_PREFIX}/login", json=login_data)
+
+    token = response.json()["access_token"]
+    headers = {"Authorization": f"Bearer {token}"}
+
+    # 읽음 표시로 변경
     for notification_id in range(1, total_fake_post_like_data_count + 1):
         response = client.patch(
             f"{settings.API_V1_PREFIX}/notifications/{notification_id}",
