e18d2c5beae00e89327710523bfa23ba04184ae1
jeha00 <rudtls0611@naver.com>
2023-10-18 11:17:42 +0900

    FEAT: add schemas about notificationd data

--- backend/sns/notification/schema.py
+++ backend/sns/notification/schema.py
@@ -12,3 +12,19 @@ class Notification(NotificationBase):
     type: NotificationType = Field(title="알림 유형")
     follow_id: int | None
     post_like_id: int | None
+
+
+class NotificationBaseData(NotificationBase):
+    type: NotificationType
+    notification_id: int
+    notified_user_id: int
+    created_at: str
+
+
+class FollowNotificationData(NotificationBaseData):
+    following_id: int
+
+
+class PostLikeNotificationData(NotificationBaseData):
+    user_id_who_like: int
+    liked_post_id: int
